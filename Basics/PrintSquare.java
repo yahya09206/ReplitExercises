52b743718a1ffde13168ed2053d57d70a17ad326
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-02 22:00:26 -0700

    print square exercise

---
+++ Basics/PrintSquare.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    //write Your code here 
+    System.out.println("*****");
+    System.out.println("*   *");
+    System.out.println("*****");
+  }
+}
