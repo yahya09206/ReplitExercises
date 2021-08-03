b5173da6290fba6ed46f0c603d3ff4dd7dfbf01a
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-02 21:54:55 -0700

    2nd replit exercise

---
+++ Basics/HelloWorld2.java
@@ -0,0 +1,7 @@
+//Write a complete main method that prints Hello world to the screen.
+public class Main {
+  public static void main(String[] args){
+    System.out.println("Hello world");
+  }
+  
+}
