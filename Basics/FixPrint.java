d1f0be1fd70ee18733834b2dec4592994600433e
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-02 21:57:59 -0700

    fix print java

---
+++ Basics/FixPrint.java
@@ -0,0 +1,6 @@
+//Rearrange the following code so that it forms a correct program that prints out the letter Q
+public class Main {
+    public static void main(String[] arg) {
+      System.out.println("Q");
+  }
+}
