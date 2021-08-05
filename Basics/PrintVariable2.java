08e562436e63f03958373815d5f7a2c0e0804425
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-04 20:32:39 -0700

    printing output of string var

---
+++ Basics/PrintVariable2.java
@@ -0,0 +1,9 @@
+/*
+Given a String variable message write a statement to disply its value
+*/
+public class PrintVariable2 {
+	public static void main(String[] args){
+		 String message = "I am a Cybertek school student.";
+		 System.out.println(message);
+	}
+}
