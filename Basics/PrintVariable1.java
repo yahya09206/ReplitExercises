cc68437fa711bfc3ce9c526770042f29ba3947e6
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-04 20:29:01 -0700

    code to print value of variable

---
+++ Basics/PrintVariable1.java
@@ -0,0 +1,9 @@
+/*
+Given an integer variable count write a statement that displays the value
+*/
+public class PrintVariable1 {
+	public static void main(String[] args){
+		int count = 100;
+		System.out.println(count);
+	}
+}
