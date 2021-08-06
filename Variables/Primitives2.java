d440badb7998e4b1f681eb1b839955bdbffbc887
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-05 22:04:00 -0700

    code to declare diff primitive types

---
+++ Variables/Primitives2.java
@@ -0,0 +1,25 @@
+/*
+Declare an byte variable named steps and assign 100
+
+Declare an short variable named miles and assign 5000
+
+Declare an int variable named count and assign 1000000
+
+Declare an long variable named population and assign 3434455667
+*/
+public class Main {
+  public static void main(String[] args) {
+    //WRITE YOUR CODE HERE:
+    byte steps = (byte)100;
+    short miles = (short)5000;
+    int count = (int)1000000;
+    long population = (long)3434455667L;
+    
+    //DO NOT TOUCH BELOW LINES. PLS 
+    System.out.println(steps);
+    System.out.println(miles);
+    System.out.println(count);
+    System.out.println(population);
+
+  }
+}
