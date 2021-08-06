9b0f22c314eb095dce0e31a716bf746b644ae027
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-05 22:05:44 -0700

    need code to print shopping list and cost

---
+++ Variables/ShoppingList1.java
@@ -0,0 +1,21 @@
+/*
+In this assignment you will write a program to create a shopping list and prices.
+
+You will use Scanner object and ask user to enter 3 items followed by its price and you will calculate total price and show as a report.
+
+-Declare 4 String variables item1, item2, item3, report.
+
+-Declare double variables price1, price2, price3, totalPrice
+
+-Create a Scanner object named scan.
+*/
+public class Main {
+  public static void main(String[] args) {
+    //WRITE YOUR CODE HERE:
+    
+    
+    
+    
+    
+  }
+}
