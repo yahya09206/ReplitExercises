04021dbb73b8f635d830e6c01e64ae10172120da
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-06 22:11:16 -0700

    print out value of sum

--- Variables/AddNumbers.java
+++ Variables/AddNumbers.java
@@ -20,8 +20,12 @@ public class AddNumbers {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter 3 numbers:");
 		int num1 = scan.next();
 		int num2 = scan.next();
 		int num3 = scan.next();
+		int sum = num1 + num2 + num3;
+		System.out.println("Sum of numbers: " + sum);
+
 	}
 }
