1c91055e1124fb67641c1824cfe6a414bdb431df
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-06 22:02:53 -0700

    code to display time

---
+++ Variables/Time.java
@@ -0,0 +1,18 @@
+/*
+In this assignment, you will write code to put together time of day.
+
+Like this: 12:24:33 PM
+
+Declare variables: hour, minute, second that can hold int values.
+
+Declare amOrPm variable that can hold a String ("AM" or "PM")
+
+Instructor will set different values to your variables
+
+Using the variables and concatenation, print values in the format mentioned above.
+*/
+public class Time {
+	public static void main(String[] args){
+		
+	}
+}
