f5764a802db2430d248be00a67c0a516516c8dd0
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-06 22:08:48 -0700

    code to add up numbers

---
+++ Variables/AddNumbers.java
@@ -0,0 +1,22 @@
+/*
+In this assignment you will create a Scanner object and ask user to enter 3 number inputs.
+
+Declare integer variables num1, num2, num3, sum.
+
+Create a Scanner object named scan.
+
+Flow:
+
+-Display prompt "Enter 3 numbers:"
+
+-Using scanner object let user input 3 numbers
+
+-Add 3 numbers and assign to sum variable
+
+-Print "Sum of numbers: ValueOfSum"
+*/
+public class AddNumbers {
+	public static void main(String[] args){
+		
+	}
+}
