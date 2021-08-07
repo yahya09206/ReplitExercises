95b658c4e68f85e09805f6eab01b4ac4ac3c288e
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-06 22:12:17 -0700

    called wrong scan object

--- Variables/AddNumbers.java
+++ Variables/AddNumbers.java
@@ -21,9 +21,9 @@ public class AddNumbers {
 
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter 3 numbers:");
-		int num1 = scan.next();
-		int num2 = scan.next();
-		int num3 = scan.next();
+		int num1 = scan.nextInt();
+		int num2 = scan.nextInt();
+		int num3 = scan.nextInt();
 		int sum = num1 + num2 + num3;
 		System.out.println("Sum of numbers: " + sum);
 
