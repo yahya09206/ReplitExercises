f29fb9fd09d6bc69836dc935d6254979da66f8be
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-07 23:52:13 -0700

    code to print patient information

---
+++ Variables/PatientInformation.java
@@ -0,0 +1,12 @@
+/*
+Declare 9 String variables firstName, lastName, fullName, email, street, state, city, address, contacts.
+
+Declare 2 int variables: age, zipcode Declare 2 double variables: height, weight. Declare boolean variable: isMarried (for marriage status). Declare 2 long variables: workPhoneNumber and personalPhoneNumber.
+
+Create a Scanner object named scan.
+*/
+public class PatientInformation {
+	public static void main(String[] args){
+		
+	}
+}
