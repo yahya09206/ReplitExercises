c3dfca803959922d758a939feb25c5bcbb041510
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-09 12:52:32 -0700

    declare scanner object

--- Variables/SchoolGrades.java
+++ Variables/SchoolGrades.java
@@ -2,3 +2,9 @@
 Write the program that will calculate the average grade. Use 5 String variables and 6 double variables to hold the subject and the grade value. Build a summary with all the information by concatenating the subjects and grades. Then calculate the average grade and print display the average score too.
 */
 import java.util.Scanner;
+public class SchoolGrades {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		
+	}
+}
