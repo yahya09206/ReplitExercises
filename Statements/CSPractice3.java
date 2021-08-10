873aaeec9074e509f5a0768604cf2f21d45166f7
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-09 21:31:38 -0700

    code to check if student or teacher

---
+++ Statements/CSPractice3.java
@@ -0,0 +1,16 @@
+/*
+The variable name holds a String user input
+
+Write a conditional statement that will determine if the person is a student or teacher.
+
+If name is equal to "Chen", print "teacher"
+For any other input, print "student"
+Examples:
+*/
+import java.util.Scanner;
+public class CSPractice3 {
+	public static void main(String[] args){
+		Scanner inp = new Scanner(System.in);
+  		String name = inp.nextLine();
+	}
+}
