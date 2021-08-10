01d39c70a3b5b4ffa8113c4dbfc02ba50117b7e6
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-09 21:08:45 -0700

    code to determine which number is greater

---
+++ Statements/GreaterNumber.java
@@ -0,0 +1,17 @@
+/*
+Use an if statement to determine if a or b is bigger. Then print:
+
+$number is greater
+
+Examples:
+*/
+import java.util.Scanner;
+public class GreaterNumber {
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+    	int a = s.nextInt();
+    	int b = s.nextInt();
+
+    	
+	}
+}
