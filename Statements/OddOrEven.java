5a3c92d91df937652ce357b9d3f95af79ecd0488
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-09 21:19:46 -0700

    code to check if odd or even

---
+++ Statements/OddOrEven.java
@@ -0,0 +1,11 @@
+/*
+Using conditional statements, check if number is odd or even. Please follow the below examples and print message accordingly:
+*/
+import java.util.Scanner;
+public class OddOrEven {
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+    	System.out.println("Enter a number:");
+    	int number = input.nextInt();
+	}
+}
