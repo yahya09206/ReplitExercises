6ca0459cca17d313f7aa038e939285d80e6b2da9
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-09 21:20:53 -0700

    done with odd or even

--- Statements/OddOrEven.java
+++ Statements/OddOrEven.java
@@ -7,5 +7,11 @@ public class OddOrEven {
 		Scanner input = new Scanner(System.in);
     	System.out.println("Enter a number:");
     	int number = input.nextInt();
+
+    	if(number % 2 == 0){
+    		System.out.println(number + " is even");
+    	}else {
+    		System.out.println(number + " is odd");
+    	}
 	}
 }
