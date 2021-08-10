df49d14dbb3ceaab27fa7d2e59107cc5ae85c235
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-09 21:44:15 -0700

    done with converting dollars to cents

--- Statements/VendingMachine.java
+++ Statements/VendingMachine.java
@@ -9,5 +9,16 @@ public class VendingMachine {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
+		int cents = scan.nextInt();
+
+		if(cents >= 0 && cents <= 100){
+			int quarters = cents / 25;
+			int dimes = (cents % 25) / 10;
+			int nickel = (cents % 25) % 10 / 5;
+			int pennies = ((cents % 25) % 10) % 5;
+			System.out.println("Your change is " + quarters + " quarters, " + dimes + " dimes, " + nickels + " nickels, and " + pennies + " pennies");
+		}else {
+			System.out.println("Invalid cents amount");
+		}
 	}
 }
