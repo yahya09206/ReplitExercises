06c8a5dcd2abbcaa5324bd56206cacb6f7a2a42f
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-09 21:25:56 -0700

    code to see if answer is correct

---
+++ Statements/Quiz.java
@@ -0,0 +1,20 @@
+/*
+In the given program you have a quiz question and a scanner to get the user input which will be stored on string a.
+
+after the user inputs an answer (a, b or c) we need to check if its the correct, wrong, or invalid answer.
+*/
+import java.util.Scanner;
+public class Quiz {
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+    
+    	System.out.println("what is the farthest planet in the solar system:" );
+    	System.out.println("a)venus" );
+    	System.out.println("b)pluto" );
+    	System.out.println("c)neptune" );
+    
+    	String a = s.nextLine();
+
+    	
+	}
+}
