af29fbe537c5c5e139d2dcbea503c62fde8a2eee
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-09 21:12:06 -0700

    code to see if two numbers are equal

---
+++ Statements/Nums.java
@@ -0,0 +1,11 @@
+/*
+You have two numbers already declared and assigned values.
+
+Check if num1 and num2 are equal, or not. Please follow the below examples and print message accordingly:
+*/
+import java.util.Scanner;
+public class Nums {
+	public static void main(String[] args){
+		
+	}
+}
