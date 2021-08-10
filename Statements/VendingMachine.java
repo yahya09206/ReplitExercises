7bd65d1ce4aaccb13902c91678e737c37ff0a7a9
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-09 21:37:20 -0700

    code to give change

---
+++ Statements/VendingMachine.java
@@ -0,0 +1,13 @@
+/*
+Write a program that will determine the change given from the vending machine. The program will accept the cents amount and output the change in quarters, dimes, nickels, and pennies.
+
+The given cents value should be more than 0 and less than 100. If the given cents is not in the range print "Invalid cents amount" and there should be no other output after
+In valid cases print the change in this format: Your change is x quarters, x dimes, x nickels, and x pennies
+*/
+import java.util.Scanner;
+public class VendingMachine {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+	}
+}
