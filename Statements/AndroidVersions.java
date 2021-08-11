dc10a0992be3744eb1b057f1da0ca87fcb21766e
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-10 19:52:03 -0700

    code to see which version of android

---
+++ Statements/AndroidVersions.java
@@ -0,0 +1,14 @@
+/*
+Android is one of the best operating systems in the world that helps the billion of users who use smart phones and tablets. Also, Android versions always had some funny names related to sweets. In this assignment, you need to write a program that will print a name of Android version, based on number. Please refer to the link beneath, in order to develop your if statement. https://www.javatpoint.com/android-versions
+
+Create an object of Scanner class named scanner. (DONE)
+Create double variable named version. (DONE)
+Write an if statement that will print android version name based on value of the version variable.
+Versions 1.5 - Cupcake 1.6 - Donut 2.1 - Eclair 2.2 - Froyo 2.3 - Gingerbread 3.1 - Honeycomb 4.0 - Ice Cream Sandwich 4.1 - Jelly Bean 4.4 - KitKat 5.0 - Lollipop 8.0 - Oreo 9.0 - Pie
+*/
+import java.util.Scanner;
+public class AndroidVersions {
+	public static void main(String[] args){
+		
+	}
+}
