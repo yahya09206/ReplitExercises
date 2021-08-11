418688807d2f50a1c7964b2ade7256bf3de9986a
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-10 20:21:48 -0700

    code to see who is old and who is not

---
+++ Statements/Citizens.java
@@ -0,0 +1,21 @@
+/*
+Declare int variables: seniorCitizens, nonSeniorCitizens, age
+Create a Scanner object
+Ask user to enter count for seniorCitizens and nonSeniorCitizens using message:
+
+"Enter current count for seniorCitizens and nonSeniorCitizens:"
+
+Ask user to enter age:
+
+"What is new citizen's age?"
+
+if citizen's age is more than or equals to 65 then print "Senior Citizen" and increment seniorCitizens variable by 1.
+
+if citizen's age is less than 65 then print "Non-Senior Citizen" and increment nonSeniorCitizens variable by 1.
+
+Last print another message:
+
+"New seniorCitizens count ValueOfVariable"
+
+"New nonSeniorCitizens count ValueOfVariable"
+*/
