38402368a06a743d03e5bfd6357dc28fe04fb80a
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-11 19:34:27 -0700

    code to check if coupons avaialble

---
+++ Statements/VideoGameCoupons.java
@@ -0,0 +1,16 @@
+/*
+The video games machines at your local arcade outputs coupons according to how well you play the game.
+
+You can redeem 10 coupons for a candy bar or you can redeem 3 for a gumball
+Write a program that displays how many candy bars and gumballs you can get. You perfer candy bars.
+
+if you the coupons are not enough for any redeem, display message:
+
+"Not enough coupons"
+*/
+import java.util.Scanner;
+public class VideoGameCoupons {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+}
