3a283ab2e94e3e54d9f67dd404c2dfd4dd0b555f
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-11 19:32:05 -0700

    done with checks

--- Statements/PosNegZero.java
+++ Statements/PosNegZero.java
@@ -5,3 +5,19 @@ Hint: use 3 if statements to do this
 
 Examples:
 */
+import java.util.Scanner;
+public class PosNegZero {
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+    	int num = s.nextInt();
+
+    	//Write your code here
+    	if (num > 0) {
+    		System.out.println("positive");
+    	}else if(num < 0){
+    		System.out.println("negative");
+    	}else {
+    		System.out.println("Zero");
+    	}
+	}
+}
