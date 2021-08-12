c689b44baeab2eadb29ecb3d8fc85b968a0d68a9
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-11 19:41:50 -0700

    forgot intro message

--- Statements/VideoGameCoupons.java
+++ Statements/VideoGameCoupons.java
@@ -12,13 +12,16 @@ import java.util.Scanner;
 public class VideoGameCoupons {
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter number of coupons:");
 		int coupons = scan.nextInt();
 
 		int candyBar = coupons / 10;
-		int gumBall = (coupons % 10) % 3;
+		int gumBall = coupons % 10 / 3;
 
 		if(coupons >= 3){
 			System.out.println("Number of Candies: " + candyBar + "\nNumber of Gumballs: " + gumBall);
+		}else {
+			System.out.println("Not enough coupons");
 		}
 	}
 }
