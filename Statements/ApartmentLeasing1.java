fab7599ba86b78e9a3c140f42f8c166ee4ea1370
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-11 19:43:53 -0700

    code to see which rooms avaiable

---
+++ Statements/ApartmentLeasing1.java
@@ -0,0 +1,15 @@
+/*
+Write program for Leasing office.
+
+numberOfBedrooms variable already declared and assigned value using Scanner:
+
+startingPrice is set to 0.
+
+prices 1 bedroom - 1100 2 bedroom - 1850 3 bedroom - 2550
+*/
+import java.util.Scanner;
+public class ApartmentLeasing1 {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+}
