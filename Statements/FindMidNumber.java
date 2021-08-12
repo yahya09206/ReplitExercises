b252bf8da36da679a95403f73a799243a86840a6
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-11 19:53:07 -0700

    code to find middle number

---
+++ Statements/FindMidNumber.java
@@ -0,0 +1,15 @@
+/*
+Write a program that will return mid number out of three numbers. No need to do any calculations.
+
+Create an object of Scanner class.
+
+Declare int 3 variables: num1, num2, num3.
+
+Example:
+*/
+import java.util.Scanner;
+public class FindMidNumber {
+	public static void main(String[] args){
+		
+	}
+}
