ca0cc3ac49914313d62d1a26a41f1ae909b12561
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-12 20:12:39 -0700

    code to check if car is recalled

---
+++ Statements/VehicleRecall.java
@@ -0,0 +1,15 @@
+/*
+SDET Motors Inc. is recalling all vehicles from model years 1995-1998, 2001-2002, 2004-2006 and 2015-2017.
+
+In this assignment you will write a program that will find vehicle for recall.
+
+If the year is in the provided range print: Your vehicle needs to be recalled!
+
+If the year is fine print: Your vehicle is fine, enjoy!
+*/
+import java.util.Scanner;
+public class VehicleRecall {
+	public static void main(String[] args){
+		
+	}
+}
