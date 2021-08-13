aa6e8f08248dc702d984ce39cb952403f7005c4a
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-12 19:48:47 -0700

    will come back

---
+++ Statements/Black.java
@@ -0,0 +1,35 @@
+/*
+In blackjack after the player asks to keep the house 4 things may happen.
+
+if the card total is bigger then 21 the player busts.
+
+if the house score is bigger then the player, the player loses .
+
+if the player score is equal to the house then they are a draw.
+
+if the player score is bigger then the house the player wins.
+
+player and house scores are represented by player and house int variables.
+
+check them using ifs to determine the result.
+*/
+import java.util.Scanner;
+public class Black {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+
+		int house = s.nextInt();
+		int player = s.nextInt();
+
+		//Write your code here:
+    	if (player > house) {
+			System.out.println("player win");
+		}else if(house + player > 21){
+			System.out.println("player bust");
+		}else if(house > player){
+			System.out.println("player loss ");
+		}else {
+			System.out.println("its a tie");
+		}
+	}
+}
