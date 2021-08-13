3b71b11f53f528803e090684fe5d5ddaa5b3fa4a
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-12 20:02:43 -0700

    code to check which program

---
+++ Statements/HappyValleySchool.java
@@ -0,0 +1,39 @@
+/*
+In the Happy Valley School System, children are classified by age as follows:
+
+Assume only positive numbers are given
+
+less than 2, ineligible
+2, toddler
+3-5, early childhood
+6-7, young reader
+8-10, elementary
+11 and 12, middle
+13, impossible
+14-16, high school
+17-18, scholar
+greater than 18, ineligible
+less than 2, ineligible
+2, toddler
+3-5, early childhood
+6-7, young reader
+8-10, elementary
+11 and 12, middle
+13, impossible
+14-16, high school
+17-18, scholar
+greater than 18, ineligible
+
+*/
+import java.util.Scanner;
+public class HappyValleySchool {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter age:");
+   		int age = input.nextInt();
+
+   		if (age < 2) {
+   			System.out.println("ineligible");
+   		}
+	}
+}
