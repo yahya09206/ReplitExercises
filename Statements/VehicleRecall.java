655c32b81f36416dcc4e78814cc216c0369bbdb4
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-12 20:20:30 -0700

    all tests passing

--- Statements/VehicleRecall.java
+++ Statements/VehicleRecall.java
@@ -10,6 +10,21 @@ If the year is fine print: Your vehicle is fine, enjoy!
 import java.util.Scanner;
 public class VehicleRecall {
 	public static void main(String[] args){
-		
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter vehicle's year:");
+		int carYear = scan.nextInt();
+
+		if (carYear == 1995 || carYear == 1996 || carYear == 1997 || carYear == 1998) {
+			System.out.println("Your vehicle needs to be recalled!");
+		}else if (carYear == 2001 || carYear == 2002) {
+			System.out.println("Your vehicle needs to be recalled!");
+		}else if (carYear == 2004 || carYear == 2005 || carYear == 2006){
+			System.out.println("Your vehicle needs to be recalled!");
+		}else if (carYear == 2015 || carYear == 2016 || carYear == 2017){
+			System.out.println("Your vehicle needs to be recalled");
+		}else {
+			System.out.println("Your vehicle is fine, enjoy!");
+		}
 	}
 }
