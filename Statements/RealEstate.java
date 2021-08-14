149aea6984a106c862520be7f464e313211e25b6
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-13 19:43:50 -0700

    prompt for garage spots

--- Statements/RealEstate.java
+++ Statements/RealEstate.java
@@ -10,7 +10,7 @@ public class RealEstate {
 		float metroAccessibility, schoolScore, highwayAccessibility;
 		boolean backyard, smoking, garage;
 		String houseType;
-		
+
 		System.out.println("* Welcome to the RealEstate calculator! *");
 		System.out.println("Enter your property type:");
 		houseType = scan.nextLine();
@@ -18,7 +18,13 @@ public class RealEstate {
 		System.out.println("How many bedrooms do you have?");
 		numberOfBedrooms = scan.nextInt();
 
+		System.out.println("Do you have a backyard?");
+		backyard = scan.nextBoolean();
 
+		System.out.println("Do you have garage?");
+		garage = nextBoolean();
+		System.out.println("How many spots?")
+		garageSpots = scan.nextInt();
 
 
 
