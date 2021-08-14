a2a93613611f02aa27a44450f217ff53aeb661e7
Yahya Hussein <yahyahussein91@gmail.com>
2021-08-13 19:39:11 -0700

    set up prompt

--- Statements/RealEstate.java
+++ Statements/RealEstate.java
@@ -1,3 +1,18 @@
 /*
 
 */
+import java.util.Scanner;
+public class RealEstate {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		int propertyPrice = 0; 
+		int numberOfBedrooms, garageSpots;
+		float metroAccessibility, schoolScore, highwayAccessibility;
+		boolean backyard, smoking, garage;
+		String houseType;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("*****************************************");
+		System.out.println("* Welcome to the RealEstate calculator! *");
+		System.out.println("*****************************************");
+	}
+}
