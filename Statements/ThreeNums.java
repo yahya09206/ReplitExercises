aa1189ca14baeddf52e81fd1d6b2e8330b49bc90
Yahya <yahyahussein91@gmail.com>
2021-08-15 16:01:46 -0700

    code to check which of three nums is bigger

---
+++ Statements/ThreeNums.java
@@ -0,0 +1,8 @@
+/*
+Write a program that checks for the biggest of 3 numbers. you get 3 int variables: n1 , n2 and n3
+
+if n1 is biggest output: "n1 is bigger"
+if n2 is biggest output: "n2 is bigger"
+if n3 is biggest output: "n3 is bigger"
+*/
+import java.util.Scanner;
