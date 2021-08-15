e6a4e437ef49801cf74cc031c4c8efcbaf3d3b34
Yahya <yahyahussein91@gmail.com>
2021-08-15 16:03:43 -0700

    check if first number is bigger

--- Statements/ThreeNums.java
+++ Statements/ThreeNums.java
@@ -9,5 +9,12 @@ import java.util.Scanner;
 public class ThreeNums {
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
+		int n1 = scan.nextInt();
+		int n2 = scan.nextInt();
+		int n3 = scan.nextInt();
+
+		if (n1 > n2 && n1 > n3) {
+			System.out.println("n1 is bigger");
+		}
 	}
 }
