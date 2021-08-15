3acc861dc104b8a6f5b7817dfb5d48d5870cbb67
Yahya <yahyahussein91@gmail.com>
2021-08-15 16:07:11 -0700

    set up class and scanner object

--- Statements/IfStatementGrades.java
+++ Statements/IfStatementGrades.java
@@ -6,3 +6,9 @@ if the grade is bigger than 70 and smaller then 90 output "good"
 if grade is bigger than 60 and smaller than 70 output "pass"
 if grade is smaller than 60 output "fail"
 */
+import java.util.Scanner;
+public class IfStatementGrades {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+}
