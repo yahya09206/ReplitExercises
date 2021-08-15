4ba433d350ac7a299f7c9c41640e334198907299
Yahya <yahyahussein91@gmail.com>
2021-08-15 16:09:15 -0700

    done with setting upm checks

--- Statements/IfStatementGrades.java
+++ Statements/IfStatementGrades.java
@@ -16,6 +16,10 @@ public class IfStatementGrades {
 			System.out.println("excellent");
 		}else if (grade > 70 && grade < 90){
 			System.out.println("good");
+		}else if (grade > 60 && grade < 70){
+			System.out.println("pass");
+		}else {
+			System.out.println("fail");
 		}
 	}
 }
