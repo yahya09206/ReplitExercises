0cb8b76c61efa3a70b265e46932f006f32449e0f
Yahya <yahyahussein91@gmail.com>
2021-08-15 16:18:14 -0700

    code to calculate tip

---
+++ Statements/TipCalculator.java
@@ -0,0 +1,9 @@
+/*
+Write a program for a tip calculator. There will be different service quality benchmarks that will determine the tip given. There will also the ability to calculate based on the number of people in the party and if there is a split of the bill or not.
+
+Poor = 5% Fair = 10% Good = 15% Great = 20% Excellent = 25%
+
+The program should display the following information based on the user input:
+
+Split or No split (Yes or No) Number of people entered: (number) (each person = & in output) Check amount: (number) Service Quality: (String) Total to pay: Total tip: Total per person: Tip per person:
+*/
