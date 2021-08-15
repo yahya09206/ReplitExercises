cda2cfd0a697df0447974616ed01c38045f32792
Yahya <yahyahussein91@gmail.com>
2021-08-15 16:11:23 -0700

    done with importing and declaring scanner object

---
+++ Statements/BurgerOrChicken.java
@@ -0,0 +1,17 @@
+/*
+A fast food company has two main order types, burger meal and chicken meal.
+
+Both have the same prices, so if a cashier enters "burger" or "chicken " he will get the same price of 10.0.
+
+If the order is burger price is 10.0
+If the order is chicken price is 10.0
+If the order is soda price is 2.0
+If the order is fries price is 3.5
+hint: or operator in java is ||
+*/
+import java.util.Scanner;
+public class BurgerOrChicken {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+}
