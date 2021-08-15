53371d54de9bb02544ad06a224056afe67e9d877
Yahya <yahyahussein91@gmail.com>
2021-08-15 16:25:25 -0700

    set up class and scanner objct

---
+++ Statements/HttpStatusCodes.java
@@ -0,0 +1,10 @@
+/*
+HTTP is the protocol that governs communications between servers and web clients (i.e. browsers). Part of the protocol includes a status code returned by the server to tell the browser the status of its most recent page request. Some of the codes and their meanings are listed below:
+Given an int variable status write a switch statement to display the status code message. If an invalid status code is entered display Invalid status code!
+*/
+import java.util.Scanner;
+public class HttpStatusCodes {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+}
