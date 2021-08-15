2fe51231281477e93a359392aa39d8f30b07a432
Yahya <yahyahussein91@gmail.com>
2021-08-15 16:05:07 -0700

    all tests are passing

--- Statements/ThreeNums.java
+++ Statements/ThreeNums.java
@@ -15,6 +15,10 @@ public class ThreeNums {
 
 		if (n1 > n2 && n1 > n3) {
 			System.out.println("n1 is bigger");
+		}else if(n2 > n1 && n2 > n3){
+			System.out.println("n2 is bigger");
+		}else {
+			System.out.println("n3 is bigger");
 		}
 	}
 }
