997618175093c986ea65d610851c6b4dd9a71771
Yahya <yahyahussein91@gmail.com>
2021-08-15 16:06:19 -0700

    code to see which grade

---
+++ Statements/IfStatementGrades.java
@@ -0,0 +1,8 @@
+/*
+Write a program that takes the grade int and prints if it's a passing grade or failure.
+
+if grade is bigger than 90 output "excellent"
+if the grade is bigger than 70 and smaller then 90 output "good"
+if grade is bigger than 60 and smaller than 70 output "pass"
+if grade is smaller than 60 output "fail"
+*/
