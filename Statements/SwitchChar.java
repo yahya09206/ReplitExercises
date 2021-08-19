8094d04b5490e6cb721e06d2d995c25de70cebfb
Yahya <yahyahussein91@gmail.com>
2021-08-18 17:30:46 -0700

    code to see which option chosen

---
+++ Statements/SwitchChar.java
@@ -0,0 +1,14 @@
+/*
+Write a switch statement that tests the value of the char variable response and performs the following actions:
+
+if response is y, the message Your request is being processed is printed
+if response is n, the message Thank you anyway for your consideration is printed
+if response is h, the message Sorry, no help is currently available is printed
+for any other value of response, the message "Invalid entry, please try again!" is printed
+Example:
+*/
+public class SwitchChar {
+	public static void main(String[] args){
+		
+	}
+}
