ab87c7be659e169bdcd60f324f0310f630191516
Yahya <yahyahussein91@gmail.com>
2021-08-19 15:53:49 -0700

    setting up prompts

--- Strings/PrintFirstAndLast.java
+++ Strings/PrintFirstAndLast.java
@@ -2,3 +2,9 @@
 Write a program that will print out first and last letters together.
 */
 import java.util.Scanner;
+public class PrintFirstAndLastLetter {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		String input = scan.next();
+	}
+}
