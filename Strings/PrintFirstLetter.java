1a6ec0e3fc17ebc42ce49b15c62eaaa4b274f680
Yahya <yahyahussein91@gmail.com>
2021-08-19 15:44:31 -0700

    prompt for word

--- Strings/PrintFirstLetter.java
+++ Strings/PrintFirstLetter.java
@@ -1,3 +1,14 @@
 /*
 Write a program that will print out the first character of the word.
 */
+import java.util.Scanner;
+
+public class PrintFirstLetter {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		String word = scan.next();
+
+		
+	}
+}
