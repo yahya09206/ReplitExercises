ebe4145667b8742a3820e0ef1aaef4dd6f915fd6
Yahya <yahyahussein91@gmail.com>
2021-08-19 15:08:18 -0700

    fixed error all tests passing

--- Statements/TernaryOperator.java
+++ Statements/TernaryOperator.java
@@ -12,6 +12,7 @@ public class TernaryOperator {
 		System.out.println("Enter number:");
 		int x = scan.nextInt();
 
-		String result2 = (x >= 5)? "x" : "-x";
+		int result2 = (x >= 5)? x : -x;
+		System.out.println(result2);
 	}	
 }
