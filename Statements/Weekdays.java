84e612484917fa94c927c623eb23b02b318906d8
Yahya <yahyahussein91@gmail.com>
2021-08-18 17:41:21 -0700

    setting up switch statement

--- Statements/Weekdays.java
+++ Statements/Weekdays.java
@@ -8,6 +8,15 @@ public class Weekdays {
 
 		System.out.println("Enter number:");
 		int num = scan.nextInt();
-		
+
+		switch(num){
+			case 1:
+				System.out.println("Monday");
+				break;
+			case 2:
+				System.out.println("Tuesday");
+				
+		}
+
 	}
 }
