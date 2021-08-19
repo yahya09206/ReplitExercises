19a9a6b4238968f4279da8dfb41a79cade7cca7a
Yahya <yahyahussein91@gmail.com>
2021-08-18 17:39:05 -0700

    import and declare scanner object

--- Statements/Weekdays.java
+++ Statements/Weekdays.java
@@ -1,3 +1,9 @@
 /*
 Write a program that will print a week days according to the day number. Use Switch statement. If an invalid day is entered print: "Not a valid day"
 */
+import java.util.Scanner;
+public class Weekdays {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+}
