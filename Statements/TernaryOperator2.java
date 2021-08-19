7f953bee2c3a1cc22156f2cd28cf966d84842fe8
Yahya <yahyahussein91@gmail.com>
2021-08-19 15:09:49 -0700

    import and declare new scanner object

---
+++ Statements/TernaryOperator2.java
@@ -0,0 +1,9 @@
+/*
+Write an expression using the conditional operator (? :) that compares the values of the variables num1 and num2. The result (that is the value) of this expression should be the value of the larger of the two variables. The larger value should be printed out.
+*/
+import java.util.Scanner;
+public class TernaryOperator2 {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+}
