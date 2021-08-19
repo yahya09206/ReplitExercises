80883161594f79577696399f0154b0b8d5eba6ad
Yahya <yahyahussein91@gmail.com>
2021-08-19 15:14:26 -0700

    done with prompts

--- Statements/ConsoleMenu.java
+++ Statements/ConsoleMenu.java
@@ -10,6 +10,14 @@ import java.util.Scanner;
 public class ConsoleMenu {
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
-		
+
+		System.out.println("---------------");
+	    System.out.println("select an option:");
+	    System.out.println("1) option 1");
+	    System.out.println("2) option 2");
+	    System.out.println("3) option 3");
+	    System.out.println("---------------");
+	    
+	    int choice = s.nextInt();
 	}
 }
