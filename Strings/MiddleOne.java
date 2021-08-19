898834c67e780112b6974e016a8dc98b3c35003b
Yahya <yahyahussein91@gmail.com>
2021-08-19 16:04:54 -0700

    set up prompt

---
+++ Strings/MiddleOne.java
@@ -0,0 +1,16 @@
+/*
+When word has odd number of characters and:
+3 or more characters, print middle letter
+Single character, print that character 3 times
+When word has even number of characters and:
+4 or more characters, print the middle 2 characters
+2 characters, print those 2 characters twice
+*/
+import java.util.Scanner;
+public class MiddleOne {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		String word = scan.next();
+
+	}
+}
