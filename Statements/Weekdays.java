e8942b35f6ba9ba3db2bc18fc47a2c38ac8409e4
Yahya <yahyahussein91@gmail.com>
2021-08-18 17:40:17 -0700

    set up prompt to capture input

--- Statements/Weekdays.java
+++ Statements/Weekdays.java
@@ -5,5 +5,9 @@ import java.util.Scanner;
 public class Weekdays {
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter number:");
+		int num = scan.nextInt();
+		
 	}
 }
