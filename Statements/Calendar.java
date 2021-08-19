6df64a75585a250fc675f280f45f91660661f29e
Yahya <yahyahussein91@gmail.com>
2021-08-18 18:05:28 -0700

    done with first three motnhs

--- Statements/Calendar.java
+++ Statements/Calendar.java
@@ -8,7 +8,18 @@ public class Calendar {
 		System.out.println("Enter month number");
      	int monthNum = scan.nextInt();
 
-     	
+
+     	switch(monthNum){
+     		case 1:
+     			System.out.println("January");
+     			break;
+     		case 2:
+     			System.out.println("February");
+     			break;
+     		case 3:
+     			System.out.println("March");
+     			break;
+     	}
 
 	}
 }
