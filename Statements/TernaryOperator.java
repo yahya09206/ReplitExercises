dda71b1f58e8747cc3063799e1a36c53f508c5e3
Yahya <yahyahussein91@gmail.com>
2021-08-19 15:01:55 -0700

    import and declare scanner object

---
+++ Statements/TernaryOperator.java
@@ -0,0 +1,13 @@
+/*
+Write an expression using the conditional operator (? :) that compares the value of the variable x to 5 and results in:
+
+Display x if x is greater than or equal to 5 Display -x if x is less than 5
+
+DO NOT USE IF STATEMENT or SWITCH CASE
+*/
+import java.util.Scanner;
+public class TernaryOperator {
+	public static void main(String[] args){
+		
+	}
+}
