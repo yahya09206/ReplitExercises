de418721ed738a1587c2c63947e17f655373f694
Yahya <yahyahussein91@gmail.com>
2021-08-19 15:38:32 -0700

    done with import and declare scanner

--- Strings/FindTheLength.java
+++ Strings/FindTheLength.java
@@ -1,3 +1,11 @@
 /*
 Write a program that will output length of the text (string).
 */
+import java.util.Scanner;
+
+public class FindTheLength {
+	public static void main(String[] args){
+		
+		Scanner scan = new Scanner(System.in);
+	}
+}
