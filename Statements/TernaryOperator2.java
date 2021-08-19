168410988c00da20827b11025e78013af755afd2
Yahya <yahyahussein91@gmail.com>
2021-08-19 15:10:19 -0700

    prompt for both numbers

--- Statements/TernaryOperator2.java
+++ Statements/TernaryOperator2.java
@@ -5,5 +5,9 @@ import java.util.Scanner;
 public class TernaryOperator2 {
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter number1:");
+		int num1 = scan.nextInt();
+		System.out.println("Enter number2:");
+		int num2 = scan.nextInt();
 	}
 }
