1030a7dcb9f365417c7cedc09df51b324fb9f57d
Yahya <yahyahussein91@gmail.com>
2021-08-19 15:39:39 -0700

    prompt for word

--- Strings/FindTheLength.java
+++ Strings/FindTheLength.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 
 public class FindTheLength {
 	public static void main(String[] args){
-		
+
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter the text:");
+		String text = scan.next();
 	}
 }
