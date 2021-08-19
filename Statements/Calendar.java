e29fdbcbb979b98b126c28fece22e4cc18200530
Yahya <yahyahussein91@gmail.com>
2021-08-18 18:04:33 -0700

    prompt for month number

--- Statements/Calendar.java
+++ Statements/Calendar.java
@@ -5,5 +5,10 @@ import java.util.Scanner;
 public class Calendar {
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter month number");
+     	int monthNum = scan.nextInt();
+
+     	
+
 	}
 }
