51540a24c7a383f1564d9d48aedb8a77561d8c58
Yahya <yahyahussein91@gmail.com>
2021-08-18 18:03:55 -0700

    import and declare new scanner object

--- Statements/Calendar.java
+++ Statements/Calendar.java
@@ -1,3 +1,9 @@
 /*
 Write a program that will print out month names by receiving a number. Use Switch Statement. If an invalid month number is entered print "Invalid month number"
 */
+import java.util.Scanner;
+public class Calendar {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+}
