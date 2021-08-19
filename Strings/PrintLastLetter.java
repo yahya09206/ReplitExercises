3d35da0ceecb71c464b7323bd5f99372925d3cb0
Yahya <yahyahussein91@gmail.com>
2021-08-19 15:49:26 -0700

    all tests passing

--- Strings/PrintLastLetter.java
+++ Strings/PrintLastLetter.java
@@ -1,3 +1,13 @@
 /*
 Write a program that will print out last letter of the word (string).
 */
+import java.util.Scanner;
+
+public class PrintLastLetter {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		String word = scan.next();
+
+		System.out.println(word.substring(word.length()-1));
+	}
+}
