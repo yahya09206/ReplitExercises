4be89c8d14c3170b8666c0dc5f59171c2d067867
Yahya <yahyahussein91@gmail.com>
2021-08-18 17:32:29 -0700

    setting up switch statmenet

--- Statements/SwitchChar.java
+++ Statements/SwitchChar.java
@@ -11,5 +11,10 @@ import java.util.Scanner;
 public class SwitchChar {
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
+		Char response = scan.nextChar();
+
+		switch(response){
+			
+		}
 	}
 }
