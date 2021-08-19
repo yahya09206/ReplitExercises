feb09856c0404ab1ed817f63342c2448b409894c
Yahya <yahyahussein91@gmail.com>
2021-08-19 15:21:17 -0700

    set up class

--- Strings/VerifyContains.java
+++ Strings/VerifyContains.java
@@ -1,3 +1,10 @@
 /*
 Write a program that will verify if the sentence contains word. Print out the result as a boolean value.
 */
+import java.util.Scanner;
+
+public class VerifyContain {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+}
