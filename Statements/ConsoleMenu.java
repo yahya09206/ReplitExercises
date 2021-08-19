6ade87a980d29d501285765c72c565217b37462b
Yahya <yahyahussein91@gmail.com>
2021-08-19 15:12:54 -0700

    code to print list of options

---
+++ Statements/ConsoleMenu.java
@@ -0,0 +1,7 @@
+/*
+A console menu is basically a text menu. After it is outputted to the console a user input is captured. Then the program decides what to do according to the user input (what the user selected).
+
+This is done using separate if statements(without else) or branched if statements.
+
+Uur console menu has 3 items called "options"
+*/
