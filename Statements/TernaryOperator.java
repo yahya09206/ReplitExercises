b0bbc3a9baa938ec74ff54f9b66875299f16da0b
Yahya <yahyahussein91@gmail.com>
2021-08-19 15:04:43 -0700

    lets see if this works

--- Statements/TernaryOperator.java
+++ Statements/TernaryOperator.java
@@ -8,6 +8,10 @@ DO NOT USE IF STATEMENT or SWITCH CASE
 import java.util.Scanner;
 public class TernaryOperator {
 	public static void main(String[] args){
-		
-	}
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter number:");
+		int x = scan.nextInt();
+
+		String result2 = (x >= 5)? "x" : "-x";
+	}	
 }
