4fc65365cab9d611d2fdef314d6a444fe3675d1e
Yahya <yahyahussein91@gmail.com>
2021-08-21 20:50:34 -0700

    code to merge two three letter words

---
+++ Strings/MergeThem.java
@@ -0,0 +1,8 @@
+/*
+You have 2 words, both of them have 3 characters.
+
+If either of them does not have exactly 3 characters, print "cannot merge"
+
+Merge their characters one by one and print together like below:
+*/
+import java.util.Scanner;
