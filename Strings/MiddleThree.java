ad5518ab716b7266bd5eaf54033759049b970136
Yahya <yahyahussein91@gmail.com>
2021-08-21 21:32:33 -0700

    code to print middle three letters

---
+++ Strings/MiddleThree.java
@@ -0,0 +1,3 @@
+/*
+Given a String variable word print the middle three characters if the word is an odd number of characters and has more than 5 characters. If the word does not meet the requirements print invalid.
+*/
