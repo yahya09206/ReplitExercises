dfdc0c118990582d74f3717613c02adfb84d638d
Yahya <yahyahussein91@gmail.com>
2021-08-21 20:47:04 -0700

    declare variables to capture inputs

--- Strings/DuplicateIt.java
+++ Strings/DuplicateIt.java
@@ -5,6 +5,9 @@ Print the first word, second word, second word, first word
 import java.util.Scanner;
 public class DuplicateIt {
 	public static void main(String[] args){
+		
 		Scanner scan = new Scanner(System.in);
+		String word1 = scan.next();
+    	String word2 = scan.next();
 	}
 }
