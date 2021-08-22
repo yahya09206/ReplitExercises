2e63e8202c94e4d8324d98c6a65a2c77f7f8554d
Yahya <yahyahussein91@gmail.com>
2021-08-21 21:04:31 -0700

    code to see if sentence contains word

---
+++ Strings/Words.java
@@ -0,0 +1,8 @@
+/*
+In this assignment you are given two string variables word1 and word2.
+Check if they are equal
+Comparison should be case sensitive. "java" and "JaVa" are not equal.
+if they are equal output "word1 equals word2"
+otherwise output "word1 does not equal word2"
+*/
+import java.util.Scanner;
