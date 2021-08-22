a5d14d2be2668e33b790d0448c8cf320dcd2089a
Yahya <yahyahussein91@gmail.com>
2021-08-21 20:46:39 -0700

    import and declare scanner object

--- Strings/DuplicateIt.java
+++ Strings/DuplicateIt.java
@@ -2,3 +2,9 @@
 You have 2 words
 Print the first word, second word, second word, first word
 */
+import java.util.Scanner;
+public class DuplicateIt {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+}
