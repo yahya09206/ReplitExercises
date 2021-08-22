b095d0613d63a91bda5d0ab7465e7deba5924d4e
Yahya <yahyahussein91@gmail.com>
2021-08-21 21:28:53 -0700

    lets see if first try works

--- Strings/StringNoEnd.java
+++ Strings/StringNoEnd.java
@@ -1,3 +1,12 @@
 /*
 Given a String txt print the value without the last letter
 */
+import java.util.Scanner;
+public class StringNoEnd {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		String txt = s.next();
+		System.out.println(txt.length() - 1);
+	}
+}
