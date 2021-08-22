6a4b604c5cdf2ab92408763ac5df0eaabb0fddc6
Yahya <yahyahussein91@gmail.com>
2021-08-21 20:51:15 -0700

    import and declare scanner object

--- Strings/MergeThem.java
+++ Strings/MergeThem.java
@@ -6,3 +6,9 @@ If either of them does not have exactly 3 characters, print "cannot merge"
 Merge their characters one by one and print together like below:
 */
 import java.util.Scanner;
+public class Merge {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+	}
+}
