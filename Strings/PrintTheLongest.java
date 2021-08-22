b9e949e4705478a54a687deff9e6431663d5e23b
Yahya <yahyahussein91@gmail.com>
2021-08-21 21:24:32 -0700

    variables to store values

--- Strings/PrintTheLongest.java
+++ Strings/PrintTheLongest.java
@@ -6,5 +6,7 @@ public class PrintTheLongest {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
+		String word1 = scan.next();
+    	String word2 = scan.next();
 	}
 }
