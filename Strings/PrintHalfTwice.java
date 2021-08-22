5b62462d725c3335ccc39ef3b443823a665db34d
Yahya <yahyahussein91@gmail.com>
2021-08-21 21:15:27 -0700

    import and declare scanner

--- Strings/PrintHalfTwice.java
+++ Strings/PrintHalfTwice.java
@@ -1,3 +1,11 @@
 /*
 Write a program that will print out first half of the word twice.
 */
+import java.util.Scanner;
+public class PrintHalfTwice {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		
+	}
+}
