4d9871cf86f72e75a40dbe1260157cd9a15a2992
Yahya <yahyahussein91@gmail.com>
2021-08-21 20:47:45 -0700

    code to print result

--- Strings/DuplicateIt.java
+++ Strings/DuplicateIt.java
@@ -5,9 +5,11 @@ Print the first word, second word, second word, first word
 import java.util.Scanner;
 public class DuplicateIt {
 	public static void main(String[] args){
-		
+
 		Scanner scan = new Scanner(System.in);
 		String word1 = scan.next();
     	String word2 = scan.next();
+
+    	System.out.println(word1 + word2 + word2 + word1);
 	}
 }
