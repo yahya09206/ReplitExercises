eb434fcf90c88210fe2c1bb5f3f304e9fd7f7f78
Yahya <yahyahussein91@gmail.com>
2021-08-21 21:06:58 -0700

    done with checks

--- Strings/Words.java
+++ Strings/Words.java
@@ -13,6 +13,11 @@ public class Words {
 		String word1 = s.next();
     	String word2 = s.next();
 
-    	
+    	if (word1.equalsIgnoreCase(word2)) {
+    		System.out.println("word1 equals word2");
+    	}else {
+    		System.out.println("word1 does not equal word2");
+    	}
+
 	}
 }
