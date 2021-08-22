17a42cb505dbda05f6633adf09dfa1868a3c0d60
Yahya <yahyahussein91@gmail.com>
2021-08-21 20:56:49 -0700

    import and declare scanner object

--- Strings/ReverseIt.java
+++ Strings/ReverseIt.java
@@ -1,3 +1,10 @@
 /*
 Write a program that will reverse a string. Your program should reverse a string only 5 characters long. If word is shorter, display message: "Too short!". If word is longer, display message: "Too long!". Otherwise, reverse this word and print out result into the console.
 */
+import java.util.Scanner;
+public class ReverseIt {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+	}
+}
