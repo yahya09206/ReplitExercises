ad78341c0f9f175dca24004ce9bd4865826d77b0
Yahya <yahyahussein91@gmail.com>
2021-08-21 20:54:18 -0700

    done with checks

--- Strings/MergeThem.java
+++ Strings/MergeThem.java
@@ -13,6 +13,10 @@ public class Merge {
 		String word1 = scan.next();
     	String word2 = scan.next();
 
-    	if(word1.length() == 3 && word2.length() == 3)
+    	if(word1.length() == 3 && word2.length() == 3){
+    		System.out.println(word1.substring(0,1) + word2.substring(0,1) + word1.substring(1,2) + word2.substring(1,2) + word1.substring(2) + word2.substring(2));
+    	}else {
+    		System.out.println("cannot merge");
+    	}
 	}
 }
