9bd20b934170ba6cd52cb1a2044475d92711842c
Yahya <yahyahussein91@gmail.com>
2021-08-21 21:24:06 -0700

    code to print longest word

---
+++ Strings/PrintTheLongest.java
@@ -0,0 +1,10 @@
+/*
+Write a program that will print out the longest word. Note: Assume the length of the two given Strings will be different
+*/
+import java.util.Scanner;
+public class PrintTheLongest {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+	}
+}
