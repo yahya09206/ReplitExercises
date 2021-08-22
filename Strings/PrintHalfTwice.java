cb449a22151c0ae3b311ad320a3695b3f861b318
Yahya <yahyahussein91@gmail.com>
2021-08-21 21:22:12 -0700

    all tests passing asked for hint

--- Strings/PrintHalfTwice.java
+++ Strings/PrintHalfTwice.java
@@ -6,6 +6,9 @@ public class PrintHalfTwice {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
-		
+		String word = scan.next();
+		int getMiddle = (word.length()) / 2;
+    	String firstHalf = word.substring(0, getMiddle);
+		System.out.println(firstHalf + firstHalf);
 	}
 }
