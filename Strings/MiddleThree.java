59a46be84f359963a00b1d2e348262bc3ab889ea
Yahya <yahyahussein91@gmail.com>
2021-08-21 21:33:35 -0700

    imported and declared scanner object

--- Strings/MiddleThree.java
+++ Strings/MiddleThree.java
@@ -1,3 +1,11 @@
 /*
 Given a String variable word print the middle three characters if the word is an odd number of characters and has more than 5 characters. If the word does not meet the requirements print invalid.
 */
+import java.util.Scanner;
+public class MiddleThree {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		String word = scan.next();
+	}
+}
