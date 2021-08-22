033b32f791ab1dc5464ef23007c42ed9d8fd781e
Yahya <yahyahussein91@gmail.com>
2021-08-21 21:25:25 -0700

    done with checks

--- Strings/PrintTheLongest.java
+++ Strings/PrintTheLongest.java
@@ -8,5 +8,11 @@ public class PrintTheLongest {
 		Scanner scan = new Scanner(System.in);
 		String word1 = scan.next();
     	String word2 = scan.next();
+
+    	if (word1.length() > word2.length()) {
+    		System.out.println(word1);
+    	}else {
+    		System.out.println(word2);
+    	}
 	}
 }
