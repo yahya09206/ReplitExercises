0adde599226d0b83d6a1eb78af33bb30f33a2480
Yahya <yahyahussein91@gmail.com>
2021-08-21 21:05:46 -0700

    vars to store input

--- Strings/Words.java
+++ Strings/Words.java
@@ -6,3 +6,13 @@ if they are equal output "word1 equals word2"
 otherwise output "word1 does not equal word2"
 */
 import java.util.Scanner;
+public class Words {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		String word1 = s.next();
+    	String word2 = s.next();
+
+    	
+	}
+}
