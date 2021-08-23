f6ddb9961264eb1210c5a5ff5489ecc3149e3f27
Yahya <yahyahussein91@gmail.com>
2021-08-22 18:29:32 -0700

    variable to store input

--- Strings/Email1.java
+++ Strings/Email1.java
@@ -1,3 +1,12 @@
 /*
 Swap first name with last name in the email (Seperated by an underscore). If the email doesn't contain an underscore print the given input email.
 */
+import java.util.Scanner;
+public class Email1 {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		String email = scan.next();
+
+	}
+}
