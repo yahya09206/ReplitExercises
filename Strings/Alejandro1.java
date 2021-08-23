1b78f8e0bc91fb4b7ec4e7977053cf515f3a5f32
Yahya <yahyahussein91@gmail.com>
2021-08-22 18:20:02 -0700

    all tests are passing

--- Strings/Alejandro1.java
+++ Strings/Alejandro1.java
@@ -1,5 +1,6 @@
 /*
 Alejandro has many emails and only has time to read the emails that refer to him. Check if his name is found in the email and if it does print "read", but if his name is not in the email print "don't read"
+use equalsIgnoreCase()
 */
 import java.util.Scanner;
 public class Alejandro1 {
