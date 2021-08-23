ff08f6ab510fa9e5f840730f60fd86829b5db3c5
Yahya <yahyahussein91@gmail.com>
2021-08-22 18:14:53 -0700

    lets see if this works

--- Strings/MiddleThree.java
+++ Strings/MiddleThree.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class MiddleThree {
 	public static void main(String[] args){
 
-		Scanner scan = new Scanner(System.in);
-		String word = scan.next();
-		String result = 
+		// Scanner scan = new Scanner(System.in);
+		//String word = scan.next();
+		String word = "Michelles";
 
 		if(word.length() % 2 != 0 && word.length() > 5){
-			System.out.println
+			System.out.println(word.substring(word.length() / 2 - 1, word.length() / 2 + 2));
 		}else {
 			System.out.println("invalid");
 		}
