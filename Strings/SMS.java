cc82d42a141bebd77ab54aeebbd38eb75ec0dc0b
Yahya <yahyahussein91@gmail.com>
2021-08-22 21:38:06 -0700

    forgot to capitalize stupid letter

--- Strings/SMS.java
+++ Strings/SMS.java
@@ -6,15 +6,14 @@ public class SMS {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
-		//String message = scan.nextLine();
-		String message = "Sender:<Mike Smith>. From Number:[202-123-3456]. Message:{I love programing and problem solving}";
+		String message = scan.nextLine();
 
-    	String sender = message.substring(message.indexOf("<") + 1, message.indexOf(">"));
-    	String phoneNumber = message.substring(message.indexOf("[") + 1, message.indexOf("]"));
-    	String messageBody = message.substring(message.indexOf("{") + 1, message.indexOf("}"));
+    	String sender = message.substring((message.indexOf("<") + 1), (message.indexOf(">")));
+    	String phoneNumber = message.substring((message.indexOf("[") + 1), (message.indexOf("]")));
+    	String messageBody = message.substring((message.indexOf("{") + 1), (message.indexOf("}")));
 
     	System.out.println("Sender: " + sender);
-    	System.out.println("Phone number: " + phoneNumber);
+    	System.out.println("Phone Number: " + phoneNumber);
     	System.out.println("Message body: " + messageBody);
 
 	}
