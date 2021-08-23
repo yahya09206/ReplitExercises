7480b9cb65e942733c9a8aa11ea68336ec81d734
Yahya <yahyahussein91@gmail.com>
2021-08-22 21:15:34 -0700

    code to print text message

---
+++ Strings/SMS.java
@@ -0,0 +1,3 @@
+/*
+Given a String message in the following format: Sender:<Mike Smith>. From Number:[202-123-3456]. Message:{I love programing and problem solving} assign the value of the sender, phoneNumber, and messageBody variables and print them.
+*/
