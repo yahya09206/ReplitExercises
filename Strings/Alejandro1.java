9c48e1f9ec1734bb1c2f7c5fed41ac19cd02b3e6
Yahya <yahyahussein91@gmail.com>
2021-08-22 18:17:54 -0700

    declare and import scanner

--- Strings/Alejandro1.java
+++ Strings/Alejandro1.java
@@ -1,3 +1,9 @@
 /*
 Alejandro has many emails and only has time to read the emails that refer to him. Check if his name is found in the email and if it does print "read", but if his name is not in the email print "don't read"
 */
+import java.util.Scanner;
+public class Alejandro1 {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+}
