ca0ef6732f5e5c266a11f4c042f3d567f527cf00
Yahya <yahyahussein91@gmail.com>
2021-08-22 18:48:19 -0700

    fixed error

--- Strings/Email1.java
+++ Strings/Email1.java
@@ -6,14 +6,15 @@ public class Email1 {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
-		String email = scan.next();
-		String firstHalf = email.substring(0,email.indexOf("_"));
-		String secondHalf = email.substring((email.indexOf("_") + 1), email.indexOf("@"));
+		//String email = scan.next();
+		String email = "seattle_washington@gmail.com";
 
-		if (!email.contains("_")) {
-			System.out.println(email);
-		}else {
+		if (email.contains("_")) {
+			String firstHalf = email.substring(0,email.indexOf("_"));
+			String secondHalf = email.substring((email.indexOf("_") + 1), email.indexOf("@"));
 			System.out.println(secondHalf + "_" + firstHalf + "@gmail.com");
+		}else {
+			System.out.println(email);
 		}
 
 	}
