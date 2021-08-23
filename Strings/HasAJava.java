ec1f4a55f3700dfa955f10c9c6bf54ae7149d00e
Yahya <yahyahussein91@gmail.com>
2021-08-23 13:17:27 -0700

    import and declared scanner obj

--- Strings/HasAJava.java
+++ Strings/HasAJava.java
@@ -1,3 +1,9 @@
 /*
 Given a string word, print true if "java" appears starting at index 0 or 1 in the string, such as with "javaxxx" or "xjavaxx" but not "xxjavaxx". The string may be any length, including 0.
 */
+import java.util.Scanner;
+public class HasAJava {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+}
