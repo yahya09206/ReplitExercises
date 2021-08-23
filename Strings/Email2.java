fcfccab51bd05e06d25b983d828dce76df12e541
Yahya <yahyahussein91@gmail.com>
2021-08-22 18:50:22 -0700

    import and declare scanner object

--- Strings/Email2.java
+++ Strings/Email2.java
@@ -1,3 +1,10 @@
 /*
 Write a program that will print out information about user based on email. Print first name, last name, and domain. First and Last name should be printed with proper format - uppercase first letter and remaining lowercase.
 */
+import java.util.Scanner;
+public class Email2 {
+	public static void main(String[] args){
+		
+		Scanner scan = new Scanner(System.in);
+	}
+}
