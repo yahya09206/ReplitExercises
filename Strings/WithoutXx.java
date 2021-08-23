1b461694ed321ad84437b48b6d57068af4ea98de
Yahya <yahyahussein91@gmail.com>
2021-08-23 13:14:56 -0700

    was doing wrong checks

--- Strings/WithoutXx.java
+++ Strings/WithoutXx.java
@@ -6,14 +6,13 @@ public class WithoutXx {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
-		//String word = scan.next();
-		String word = "XJohnx";
+		String word = scan.next();
 
-		if (word.startsWith("x") || word.startsWith("X") && word.endsWith("x") || word.endsWith("X")) {
-			System.out.println(word.substring(1, word.length() - 1));
-		}else if(word.startsWith("x") || word.startsWith("X")){
+		if (word.startsWith("x") && word.endsWith("X")) {
+			System.out.println(word.substring(1, word.indexOf("X")));
+		}else if(word.startsWith("x")){
 			System.out.println(word.substring(1));
-		}else if(word.endsWith("x") || word.endsWith("X")){
+		}else if(word.endsWith("X")){
 			System.out.println(word.substring(0, word.length() - 1));
 		}else {
 			System.out.println(word);
