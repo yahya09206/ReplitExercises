6f8aa768283e93faf5b2c66a2e4527cd6319e210
Yahya <yahyahussein91@gmail.com>
2021-08-22 21:14:04 -0700

    all tests passing

--- Strings/Email2.java
+++ Strings/Email2.java
@@ -11,8 +11,8 @@ public class Email2 {
 		String firstName = email.substring(0, email.indexOf("_"));
 		String lastName = email.substring(email.indexOf("_") + 1, email.indexOf("@"));
 
-		System.out.println(firstName.toUpperCase().charAt(0) + email.substring(1, email.indexOf("_")));
-		System.out.println(lastName.toUpperCase().charAt(0) + email.substring(email.indexOf("_") + 2, email.indexOf("@")));
-		System.out.println(email.substring(email.indexOf("@") + 1, email.indexOf(".")));
+		System.out.println("First name: " + firstName.toUpperCase().charAt(0) + email.substring(1, email.indexOf("_")));
+		System.out.println("Last name: " + lastName.toUpperCase().charAt(0) + email.substring(email.indexOf("_") + 2, email.indexOf("@")));
+		System.out.println("Domain: " + email.substring(email.indexOf("@") + 1, email.indexOf(".")));
 	}
 }
