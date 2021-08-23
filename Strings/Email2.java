f344ffddb860d173b9e226925ab18985f07ecff0
Yahya <yahyahussein91@gmail.com>
2021-08-22 18:50:55 -0700

    variable to store input

--- Strings/Email2.java
+++ Strings/Email2.java
@@ -4,7 +4,8 @@ Write a program that will print out information about user based on email. Print
 import java.util.Scanner;
 public class Email2 {
 	public static void main(String[] args){
-		
+
 		Scanner scan = new Scanner(System.in);
+		String email = scan.next();
 	}
 }
