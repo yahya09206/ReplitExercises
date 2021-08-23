a4d0606c65c5bbadf9bf6c90911f3bbd0318d131
Yahya <yahyahussein91@gmail.com>
2021-08-22 18:17:05 -0700

    code to see if email contains name

---
+++ Strings/Alejandro1.java
@@ -0,0 +1,3 @@
+/*
+Alejandro has many emails and only has time to read the emails that refer to him. Check if his name is found in the email and if it does print "read", but if his name is not in the email print "don't read"
+*/
