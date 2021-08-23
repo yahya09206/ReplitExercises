fa5268ba977d842c80ba717d5e1526415aaf7b5c
Yahya <yahyahussein91@gmail.com>
2021-08-22 18:49:45 -0700

    code to print words of email seperately

---
+++ Strings/Email2.java
@@ -0,0 +1,3 @@
+/*
+Write a program that will print out information about user based on email. Print first name, last name, and domain. First and Last name should be printed with proper format - uppercase first letter and remaining lowercase.
+*/
