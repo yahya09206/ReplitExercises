533e2f2e00379884171c0ee9d48333fe02995678
Yahya <yahyahussein91@gmail.com>
2021-08-23 13:16:44 -0700

    code to see if word contains java

---
+++ Strings/HasAJava.java
@@ -0,0 +1,3 @@
+/*
+Given a string word, print true if "java" appears starting at index 0 or 1 in the string, such as with "javaxxx" or "xjavaxx" but not "xxjavaxx". The string may be any length, including 0.
+*/
