8f5e88e0d21ebbd7784d0fcd1451c1fc23a80a78
Yahya <yahyahussein91@gmail.com>
2021-08-22 18:15:51 -0700

    all tests are passing

--- Strings/MiddleThree.java
+++ Strings/MiddleThree.java
@@ -5,15 +5,13 @@ import java.util.Scanner;
 public class MiddleThree {
 	public static void main(String[] args){
 
-		// Scanner scan = new Scanner(System.in);
-		//String word = scan.next();
-		String word = "Michelles";
+		Scanner scan = new Scanner(System.in);
+		String word = scan.next();
 
 		if(word.length() % 2 != 0 && word.length() > 5){
 			System.out.println(word.substring(word.length() / 2 - 1, word.length() / 2 + 2));
 		}else {
 			System.out.println("invalid");
 		}
-
 	}
 }
