ba02eb8eebd0a3915313491155653da05c64dcbe
Yahya <yahyahussein91@gmail.com>
2021-08-22 18:21:21 -0700

    code to check if two words are contained

---
+++ Strings/Alejandro2.java
@@ -0,0 +1,3 @@
+/*
+Alejandro has many emails and only has time to read the emails that refer to him and a project. Check if his name is found in the email and if project is also included and if it does print "read", but if his name is not in the email print "don't read"
+*/
