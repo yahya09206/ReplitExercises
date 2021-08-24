93a71341984e2a234be8b50c68d37f315bcf362a
Yahya <yahyahussein91@gmail.com>
2021-08-24 06:03:28 -0700

    imported and declared scanner object

---
+++ Strings/GetSandwich.java
@@ -0,0 +1,9 @@
+/*
+A sandwich is two pieces of bread with something in between. Print the string that is between the first and last appearance of "bread" in the given string, or return string "nothing" if there are not two pieces of bread.
+*/
+import java.util.Scanner;
+public class GetSandwich {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+}
