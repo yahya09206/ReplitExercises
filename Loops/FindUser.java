150398158582482036c639536551f03aad8c1017
Yahya <yahyahussein91@gmail.com>
2021-08-24 12:14:08 -0700

    code to find user name

---
+++ Loops/FindUser.java
@@ -0,0 +1,4 @@
+/*
+Write a program that will look up a user.
+Assume that you have only 2 users: Max Payne and Alan Wake. First, ask user to enter full name. Display message: "Enter full name:". Then take input from user. If name is equals to either "Max Payne" or "Alan Wake", display message: "User found!". Otherwise, display message: "User not found!". Please make your search case insensitive!
+*/
