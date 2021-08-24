2b4dc4a2d54cd6a0666ce7471d2dcff8ea0c72a6
Yahya <yahyahussein91@gmail.com>
2021-08-24 12:18:17 -0700

    prompt for guest name

--- Loops/Party.java
+++ Loops/Party.java
@@ -10,5 +10,7 @@ public class Party {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter guest name:");
+		String name = scan.next();
 	}
 }
