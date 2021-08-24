3a307cf0b13bcdade01c370072ea1b69eccc64c0
Yahya <yahyahussein91@gmail.com>
2021-08-24 06:34:54 -0700

    import and delcare scanner

--- Loops/ShoppingList2.java
+++ Loops/ShoppingList2.java
@@ -3,3 +3,10 @@ Your program should ask use to enter items followed by its price. After adding i
 
 Hint: use do while loop.
 */
+import java.util.Scanner;
+public class ShoppingList2 {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+	}
+}
