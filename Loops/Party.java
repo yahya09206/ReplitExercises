7065e5d218576ae4e3a70e77a756dd91b797d0bd
Yahya <yahyahussein91@gmail.com>
2021-08-24 12:16:46 -0700

    code to construct list of guest names

---
+++ Loops/Party.java
@@ -0,0 +1,7 @@
+/*
+Imagine you have a party and need to form the list of the guests. (Assume you have at least one guest)
+Ask for the first guest name.
+Then ask does user want to enter one more guests.
+If yes - take the next guest input
+If not - finish the program and print list of the guests.
+*/
