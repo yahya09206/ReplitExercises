40897e7bebd2452c501b48192b95923b77a5e5c4
Yahya <yahyahussein91@gmail.com>
2021-08-24 06:35:37 -0700

    set up variables

--- Loops/ShoppingList2.java
+++ Loops/ShoppingList2.java
@@ -8,5 +8,11 @@ public class ShoppingList2 {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
+		String shoppingListReport = "";
+		String item = "";
+		String countinue = "";
+		double price = 0;
+		int count = 1;
+		double totalPrice = 0;
 	}
 }
