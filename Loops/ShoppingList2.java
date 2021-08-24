f9124670e1bd5157ce15c4ab31028d1b64a99a52
Yahya <yahyahussein91@gmail.com>
2021-08-24 06:34:06 -0700

    code to print shopping list using do while loop

---
+++ Loops/ShoppingList2.java
@@ -0,0 +1,5 @@
+/*
+Your program should ask use to enter items followed by its price. After adding item, ask user if he wants to add one more item. If so, repeat previous steps again. If no, print shopping list report and total price as show in examples. Your program should accept up to 10 items.
+
+Hint: use do while loop.
+*/
