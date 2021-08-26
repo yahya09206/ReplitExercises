055233a915a2e33f120c28c7b9bd6bcc6b9e1ca5
Yahya <yahyahussein91@gmail.com>
2021-08-25 21:21:02 -0700

    code to decrease population by half

---
+++ Loops/Inhabitants.java
@@ -0,0 +1,3 @@
+/*
+A variable inhabitants represents a city and its respective populations. City might have a population of 0 due to a pandemic zombie disease that is wiping away the human lives. After each day, a city will lose half of its population. Write a program to loop the city population and make it lose half of its population until population is zero. Print the each day like below for each day:
+*/
