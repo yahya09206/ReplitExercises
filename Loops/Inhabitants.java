aa3afec41808c818044571dba205a7b42d4181cb
Yahya <yahyahussein91@gmail.com>
2021-08-25 21:36:54 -0700

    setting up else statmenet

--- Loops/Inhabitants.java
+++ Loops/Inhabitants.java
@@ -9,12 +9,16 @@ public class Inhabitants {
 		int inhabitants = scan.nextInt();
 		int day = 0;
 
-		if (inhabitants > 0) {
+		if (inhabitants != 0) {
 			do {
 				System.out.println("Day " + day + " [" + inhabitants + "]");
 				day++;
 				inhabitants /= 2;
-			}
+			}while(inhabitants > 0);
+			System.out.println("----EXTINCT----");
+		}else {
+			
 		}
+
 	}
 }
