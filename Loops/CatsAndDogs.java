b125d49f88232e8bd91e831ba9efb0f5f537ba3f
Yahya <yahyahussein91@gmail.com>
2021-08-26 19:46:01 -0700

    var to keep track of character count

--- Loops/CatsAndDogs.java
+++ Loops/CatsAndDogs.java
@@ -8,6 +8,8 @@ public class CatsAndDogs {
 		int countOfCats = 0;
     	int countOfDogs = 0;
     	String word = scan.next();
+    	int charCount = word.length();
+    	int tCharCount = 3; // character count of target word we are looking for
 
     	for (int i = 0; i <= word.length() - 1; i++) {
     		
