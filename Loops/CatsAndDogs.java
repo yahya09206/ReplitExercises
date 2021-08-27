9122d82a83d8a63ea08343da0326ce872a11dad2
Yahya <yahyahussein91@gmail.com>
2021-08-26 19:39:16 -0700

    setting up for loop to go through word

--- Loops/CatsAndDogs.java
+++ Loops/CatsAndDogs.java
@@ -3,9 +3,14 @@ Print true if the string "cat" and "dog" appear the same number of times in the 
 */
 public class CatsAndDogs {
 	public static void main(String[] args){
+
 		Scanner scan = new Scanner(System.in);
 		int countOfCats = 0;
     	int countOfDogs = 0;
     	String word = scan.next();
+
+    	for (int i = 0; i < word.length(); i++) {
+    		
+    	}
 	}
 }
