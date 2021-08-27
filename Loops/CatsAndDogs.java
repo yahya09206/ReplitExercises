0feed7658bd20d963b917cf23324f2e13c8bc669
Yahya <yahyahussein91@gmail.com>
2021-08-26 19:48:42 -0700

    testing with dummy data

--- Loops/CatsAndDogs.java
+++ Loops/CatsAndDogs.java
@@ -1,18 +1,20 @@
 /*
 Print true if the string "cat" and "dog" appear the same number of times in the given string word.
 */
+import java.util.Scanner;
 public class CatsAndDogs {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
 		int countOfCats = 0;
     	int countOfDogs = 0;
-    	String word = scan.next();
-    	int charCount = word.length();
-    	int tCharCount = 3; // character count of target word we are looking for
+    	//String word = scan.next();
+    	String word = "ilovejava";
+    	int wordcharCount = word.length();
+    	int tCharCount = 5; // character count of target word we are looking for
 
-    	for (int i = 0; i <= word.length() - 1; i++) {
-    		
+    	for (int i = 0; i <= wordcharCount - tCharCount; i++) {
+    		System.out.println(word.substring(i, i + tCharCount));
     	}	
 	}
 }
