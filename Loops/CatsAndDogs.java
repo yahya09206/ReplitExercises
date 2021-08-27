d12c1ac4384b2a7d1a60fbfcf52cc5e983be859e
Yahya <yahyahussein91@gmail.com>
2021-08-26 19:37:56 -0700

    import and declare scanners and vars

---
+++ Loops/CatsAndDogs.java
@@ -0,0 +1,11 @@
+/*
+Print true if the string "cat" and "dog" appear the same number of times in the given string word.
+*/
+public class CatsAndDogs {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		int countOfCats = 0;
+    	int countOfDogs = 0;
+    	String word = scan.next();
+	}
+}
