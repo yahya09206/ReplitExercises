750b14e187f05f28ae024b2c8175dde5021f7758
Yahya <yahyahussein91@gmail.com>
2021-08-26 20:04:17 -0700

    multiplying but correct result

--- Loops/Factorial.java
+++ Loops/Factorial.java
@@ -6,11 +6,13 @@ public class Factorial {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
+		int n = 5;
 		long result = 1L;
 
 		for (int i = n; i >= 1; i--) {
-			result *= i ;
+			result *= i;
 		}
+
+		System.out.println(result + " ");
 	}
 }
