91d6f21e66d2234c20d9d49925b93be1105901d7
Yahya <yahyahussein91@gmail.com>
2021-08-26 20:05:51 -0700

    all tests are passing

--- Loops/Factorial.java
+++ Loops/Factorial.java
@@ -6,7 +6,7 @@ public class Factorial {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
-		int n = 5;
+		int n = scan.nextInt();
 		long result = 1L;
 
 		for (int i = n; i >= 1; i--) {
