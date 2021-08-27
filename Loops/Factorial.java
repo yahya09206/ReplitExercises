a311b38364e6b73ece4b0100a53f745aaf9e1f86
Yahya <yahyahussein91@gmail.com>
2021-08-26 19:58:25 -0700

    set up class template

--- Loops/Factorial.java
+++ Loops/Factorial.java
@@ -1,3 +1,9 @@
 /*
 In mathematics a factorial of a positive integer n, denoted by n!, is the product of all positive integers less than or equal to n. Calculate factorial and output result to the console.
 */
+import java.util.Scanner;
+public class Factorial {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+}
