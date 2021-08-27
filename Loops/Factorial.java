d16da60cbd9cdc9d9ea99db9ff5be4dbab7c0722
Yahya <yahyahussein91@gmail.com>
2021-08-26 19:57:48 -0700

    code to print factorial of number

---
+++ Loops/Factorial.java
@@ -0,0 +1,3 @@
+/*
+In mathematics a factorial of a positive integer n, denoted by n!, is the product of all positive integers less than or equal to n. Calculate factorial and output result to the console.
+*/
