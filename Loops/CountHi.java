2afe027dae02831cb54243f05856f7bca16bac85
Yahya <yahyahussein91@gmail.com>
2021-08-27 20:31:39 -0700

    imported and declared scanner

--- Loops/CountHi.java
+++ Loops/CountHi.java
@@ -1,3 +1,10 @@
 /*
 Print out the number of times that the string "hi" appears anywhere in the given string. Only lowercase "hi" should be counted.
 */
+import java.util.Scanner;
+public class CountHi {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+	}
+}
