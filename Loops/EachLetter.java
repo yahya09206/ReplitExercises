78b0a2df30001e8c06a9e86d391752cb3e1499ea
Yahya <yahyahussein91@gmail.com>
2021-08-27 21:05:30 -0700

    all tests passing

--- Loops/EachLetter.java
+++ Loops/EachLetter.java
@@ -6,5 +6,10 @@ public class EachLetter {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
+		String word = scan.nextLine();
+
+		for (int i = 0; i <= word.length() - 1; i++) {
+			System.out.println(word.charAt(i));
+		}
 	}
 }
