d8e57c15145b699fe0e0a75a52a710109eefa902
Yahya <yahyahussein91@gmail.com>
2021-08-27 20:48:15 -0700

    import and declare scanner

--- Loops/CountTriples.java
+++ Loops/CountTriples.java
@@ -1,3 +1,10 @@
 /*
 We'll say that a "triple" in a string is a char appearing three times in a row. Print out the number of triples in the given string. The triples may overlap.
 */
+import java.util.Scanner;
+public class CountTriple {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+	}
+}
