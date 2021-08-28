5a478404ffb92358bc3735a1649dce8d016bb46b
Yahya <yahyahussein91@gmail.com>
2021-08-27 21:03:33 -0700

    import and declared scanner

--- Loops/EachLetter.java
+++ Loops/EachLetter.java
@@ -1,3 +1,10 @@
 /*
 Given a String, use a loop to print every character from the String on a new line.
 */
+import java.util.Scanner;
+public class EachLetter {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+	}
+}
