e3236baae76c458f65c34f70b02ffdad5b0449c3
Yahya <yahyahussein91@gmail.com>
2021-08-27 21:07:39 -0700

    import and declare scanner with var to store input

---
+++ Loops/EqualsJavaPython.java
@@ -0,0 +1,11 @@
+/*
+Given a string, print out true if the number of appearances of "java" anywhere in the string is equal to the number of appearances of "python" anywhere in the string (case sensitive).
+*/
+import java.util.Scanner;
+public class EqualsJavaPython {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		String sentence = scan.nextLine();
+	}
+}
