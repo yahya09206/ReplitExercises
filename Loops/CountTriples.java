6b093168a2373f6f3a8878346f9fc1b2971c2342
Yahya <yahyahussein91@gmail.com>
2021-08-27 20:51:07 -0700

    if statement to check

--- Loops/CountTriples.java
+++ Loops/CountTriples.java
@@ -8,5 +8,13 @@ public class CountTriple {
 		Scanner scan = new Scanner(System.in);
 		String str = scan.next();
     	int count = 0 ;
+
+    	for (int i = 0; i < str.length(); i++) {
+    		if (word.charAt(i) == word.charAt(i)) {
+    			count++
+    		}else {
+    			count = 0;
+    		}
+    	}
 	}
 }
