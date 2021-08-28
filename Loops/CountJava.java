098826058efa05995086dfb086a6a846be006d06
Yahya <yahyahussein91@gmail.com>
2021-08-27 20:39:59 -0700

    vars to store count and length

--- Loops/CountJava.java
+++ Loops/CountJava.java
@@ -1,3 +1,14 @@
 /*
 Print the number of times that the string "java" appears anywhere in the given string word
 */
+import java.util.Scanner;
+public class CountJava {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		String word = scan.next();
+		int wordCount = word.length();
+		int wordcharCount = 4;
+		int javaCount = scan.nextInt();
+	}
+}
