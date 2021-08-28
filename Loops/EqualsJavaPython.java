db764d68a311509c5eb35181c5cc9a30ba9cb97d
Yahya <yahyahussein91@gmail.com>
2021-08-27 21:09:22 -0700

    condition to check if both words exist

--- Loops/EqualsJavaPython.java
+++ Loops/EqualsJavaPython.java
@@ -7,5 +7,15 @@ public class EqualsJavaPython {
 
 		Scanner scan = new Scanner(System.in);
 		String sentence = scan.nextLine();
+		boolean result = true;
+
+		for (int i = 0; i <= sentence.length() - 1; i++) {
+			if (sentence.contains("java") && sentence.contains("python")) {
+				result = result;
+			}else {
+				result = false;
+			}
+		}
+		System.out.println(result);
 	}
 }
