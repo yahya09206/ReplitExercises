6065aa87fcc8e131ac546dc87e5e17c6d72d5106
Yahya <yahyahussein91@gmail.com>
2021-08-29 21:14:35 -0700

    variable to store input

--- Loops/PrintNTimes.java
+++ Loops/PrintNTimes.java
@@ -4,7 +4,10 @@ Given an int variable n print out n asterisks. So if n=5, five asterisks will be
 import java.util.Scanner;
 public class PrintNTimes {
 	public static void main(String[] args){
-		
+
 		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+
+		
 	}
 }
