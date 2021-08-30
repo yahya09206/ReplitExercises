e510a64ade6d1afe0875107034685d6366ff3368
Yahya <yahyahussein91@gmail.com>
2021-08-29 21:29:15 -0700

    ending and starting char

--- Loops/Letters.java
+++ Loops/Letters.java
@@ -6,5 +6,7 @@ public class Letters {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
+		char start = scan.next().charAt(0);
+    	char end = scan.next().charAt(0);
 	}
 }
