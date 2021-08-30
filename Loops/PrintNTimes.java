2b4de73565c50412e88911d247d244d0c9b8549b
Yahya <yahyahussein91@gmail.com>
2021-08-29 21:14:07 -0700

    import and declared scanner

--- Loops/PrintNTimes.java
+++ Loops/PrintNTimes.java
@@ -1,3 +1,10 @@
 /*
 Given an int variable n print out n asterisks. So if n=5, five asterisks will be printed.
 */
+import java.util.Scanner;
+public class PrintNTimes {
+	public static void main(String[] args){
+		
+		Scanner scan = new Scanner(System.in);
+	}
+}
