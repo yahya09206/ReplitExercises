6a2cf588c82f1a99a386f04a0fb70311511e16b8
Yahya <yahyahussein91@gmail.com>
2021-08-29 21:26:52 -0700

    import and declared scanner

---
+++ Loops/Letters.java
@@ -0,0 +1,10 @@
+/*
+Write a program that will print out letters in the alphabetic order accordingly to the given range within 2 chars.
+*/
+import java.util.Scanner;
+public class Letters {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+	}
+}
