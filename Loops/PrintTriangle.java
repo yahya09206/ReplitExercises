b78a91193d5351ba1d6c936b0fdaee15a9f6ce90
Yahya <yahyahussein91@gmail.com>
2021-08-29 21:35:23 -0700

    import and declare scanner

--- Loops/PrintTriangle.java
+++ Loops/PrintTriangle.java
@@ -1,3 +1,10 @@
 /*
 Write a program that will create a triangle of asterisks based on size n.
 */
+import java.util.Scanner;
+public class PrintTriangle {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+	}
+}
