40f32d25bb0e62b0a24b6b5cc0c4b46b9955fe14
Yahya <yahyahussein91@gmail.com>
2021-08-29 21:17:42 -0700

    imported and declared scanner object

--- Loops/PrintEvenFrom80To20.java
+++ Loops/PrintEvenFrom80To20.java
@@ -1,3 +1,10 @@
 /*
 Write a for loop that prints all even integers from 80 through and including 20. Seperate each number with a space
 */
+import java.util.Scanner;
+public class PrintEvenFrom80To20 {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+	}
+}
