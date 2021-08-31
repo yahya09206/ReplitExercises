9ba0153f08485e34fcee2b9117ee700f0a601085
Yahya <yahyahussein91@gmail.com>
2021-08-30 22:08:56 -0700

    import and declare scanner

--- Loops/RepeatSeperator.java
+++ Loops/RepeatSeperator.java
@@ -1,3 +1,10 @@
 /*
 Given two strings, word and a separator sep, return a big string made of count occurrences of the word, separated by the separator string.
 */
+import java.util.Scanner;
+public class RepeatSeperator {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+	}
+}
