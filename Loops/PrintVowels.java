bd724bdfb48e9e32e6f5c36096cbdeb239f9ab03
Yahya <yahyahussein91@gmail.com>
2021-08-30 22:00:40 -0700

    imported and declared scanner

--- Loops/PrintVowels.java
+++ Loops/PrintVowels.java
@@ -1,3 +1,10 @@
 /*
 Create a program that will take the given String In and print out all the vowels from the String.
 */
+import java.util.Scanner;
+public class PrintVowels {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+	}
+}
