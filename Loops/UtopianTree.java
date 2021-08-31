5ebc87078a067ddcce1a5322f9773cbd8d3e1023
Yahya <yahyahussein91@gmail.com>
2021-08-30 22:21:35 -0700

    vars to track growth and count

--- Loops/UtopianTree.java
+++ Loops/UtopianTree.java
@@ -1,3 +1,12 @@
 /*
 The Utopian Tree grows exactly 1 cm for the first three years, and after that it grows by 2 cm every year. Show 10 years of growth of the Utopian Tree.
 */
+import java.util.Scanner;
+public class UtopianTree {
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		int growth = 0;
+		int count = 1;
+	}
+}
