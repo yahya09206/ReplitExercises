0c2762c32c9bec4e6e52d1f7c21b0e0e5d869331
Yahya <yahyahussein91@gmail.com>
2021-08-30 22:09:39 -0700

    vars to store count word and seperator

--- Loops/RepeatSeperator.java
+++ Loops/RepeatSeperator.java
@@ -6,5 +6,8 @@ public class RepeatSeperator {
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
+		String word = scan.next();
+    	String separator = scan.next();
+    	int count = scan.nextInt();
 	}
 }
