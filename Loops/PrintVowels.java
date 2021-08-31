62b43bc0fe88b756d4a2bcfdd5abfd397b37cc47
Yahya <yahyahussein91@gmail.com>
2021-08-30 22:05:35 -0700

    var to store result

--- Loops/PrintVowels.java
+++ Loops/PrintVowels.java
@@ -7,12 +7,6 @@ public class PrintVowels {
 
 		Scanner scan = new Scanner(System.in);
 		String word = scan.nextLine();
-		char vowels = 'aeiou';
-
-		for (int i = 0; i <= word.length() - 1; i++) {
-			if (word.charAt(i).contains(vowels)) {
-				System.out.println(word.charAt(i));
-			}
-		}
+		String result = "";
 	}
 }
