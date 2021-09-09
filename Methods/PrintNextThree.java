94f3cc1941bd6d37825d72087b78b3d3567140c1
Yahya <yahyahussein91@gmail.com>
2021-09-08 18:23:09 -0700

    first attempt didnt work

--- Methods/PrintNextThree.java
+++ Methods/PrintNextThree.java
@@ -1,3 +1,17 @@
 /*
 Create a method called next3 . This method has an int argument and prints the next 3 numbers after that number. Call the method from main method and pass num to it.
 */
+import java.util.Scanner;
+public class PrintNextThree {
+	public static void main(String[] args){
+		Scanner inp = new Scanner(System.in);
+    	System.out.print("enter number");
+    	int num = inp.nextInt();
+		next3(num);
+	}
+	public static void next3(int num){
+		num++;
+		num++;
+		num++;
+	}
+}
