ef60e38a3a8ecc05ba403469b75cc6a28340c7a0
Yahya <yahyahussein91@gmail.com>
2021-09-08 18:02:46 -0700

    calling world method

--- Methods/HelloWorld.java
+++ Methods/HelloWorld.java
@@ -4,6 +4,7 @@ Given 2 static methods hello() and world(). Provide an implementation for both o
 public class Main {
 	public static void main(String[] args){
 		hello();
+		world();
 	}
 	public static void hello(){
 		System.out.println("Hello");
