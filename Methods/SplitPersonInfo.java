13acd8654eee173e7baf4a8b1f67ebc61d6b8400
Yahya <yahyahussein91@gmail.com>
2021-09-08 18:37:43 -0700

    calling method with params

--- Methods/SplitPersonInfo.java
+++ Methods/SplitPersonInfo.java
@@ -1,3 +1,10 @@
 /*
 The method person has a string arguement with this format: "name,last name,age". Print out the person's information
 */
+import java.util.Scanner;
+public class SplitPersonInfo {
+	public static void main(String[] args){
+
+		person("Michael, Williams, 30");
+	}
+}
