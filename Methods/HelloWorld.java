244dda26f1758ba0dbe98c36e13044218a936e3c
Yahya <yahyahussein91@gmail.com>
2021-09-08 18:00:49 -0700

    code to add two methods and print hello world

---
+++ Methods/HelloWorld.java
@@ -0,0 +1,3 @@
+/*
+Given 2 static methods hello() and world(). Provide an implementation for both of them in order to print appropriate message. So hello() method should print "Hello" and world() method should print "World!".`
+*/
