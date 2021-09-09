4f2510710a449d821e92c146e6372a40d6080f40
Yahya <yahyahussein91@gmail.com>
2021-09-08 18:47:03 -0700

    var to store result

---
+++ Methods/PopulateArray.java
@@ -0,0 +1,13 @@
+/*
+The populate method accepts an empty int array and populates it with numbers counting up.
+*/
+import java.util.Scanner;
+public class PopulateArray {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+	public static int[] populate(int[] r){
+		int[] result;
+		
+	}
+}
