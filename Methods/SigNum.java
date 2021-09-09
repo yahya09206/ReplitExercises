533e090eac0e619bc506b9475d2b10a9ea0c9646
Yahya <yahyahussein91@gmail.com>
2021-09-08 18:16:45 -0700

    checking if positive

--- Methods/SigNum.java
+++ Methods/SigNum.java
@@ -1,3 +1,15 @@
 /*
 Given a method sign write the code so that its takes a number arguement and tells you if its positive, negative or zero.
 */
+import java.util.Scanner;
+public class SigNum {
+	public static void main(String[] args){
+		sign(n);
+	}
+	public static void sign(int n){
+
+		if (n > 0) {
+			System.out.println("positive")
+		}
+	}
+}
