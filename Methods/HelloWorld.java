b942f319f0fa129ce162eb7d68fd0ecad8653588
Yahya <yahyahussein91@gmail.com>
2021-09-08 18:01:46 -0700

    first method to print hello

--- Methods/HelloWorld.java
+++ Methods/HelloWorld.java
@@ -1,3 +1,11 @@
 /*
 Given 2 static methods hello() and world(). Provide an implementation for both of them in order to print appropriate message. So hello() method should print "Hello" and world() method should print "World!".`
 */
+public class Main {
+	public static void main(String[] args){
+
+	}
+	public static void hello(){
+		System.out.println("Hello");
+	}
+}
