1195822591552d9e5431fd2c6d61ced8462bf6a7
Yahya <yahyahussein91@gmail.com>
2021-09-08 18:10:25 -0700

    code to print cubed version of an int

---
+++ Methods/FindCube.java
@@ -0,0 +1,3 @@
+/*
+Create a method called cube. It is a void method with no arguments. Write all required code inside this method in order to read a number from the console and then prints the cubed value of that number:
+*/
