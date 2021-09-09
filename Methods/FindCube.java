b2d171ee0dc71ed98572799da7ce9fdf204da94b
Yahya <yahyahussein91@gmail.com>
2021-09-08 18:11:53 -0700

    prompting for num

--- Methods/FindCube.java
+++ Methods/FindCube.java
@@ -1,3 +1,13 @@
 /*
 Create a method called cube. It is a void method with no arguments. Write all required code inside this method in order to read a number from the console and then prints the cubed value of that number:
 */
+import java.util.Scanner;
+public class FindCube {
+	public static void main(String[] args){
+		cube();
+	}
+	public static void cube(){
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+	}
+}
