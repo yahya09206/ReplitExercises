2d900b19c4bb2b81e29875d518c7af918c365a38
Yahya <yahyahussein91@gmail.com>
2021-09-08 18:54:39 -0700

    code to switch first and last element

---
+++ Methods/SwitchElements.java
@@ -0,0 +1,18 @@
+/*
+Implement the do_switch method Switch the last element in an array with the first and return the array.
+*/
+import java.util.Arrays;
+import java.util.Scanner;
+class Main {
+
+  public static void main(String [] args) {
+    Scanner in = new Scanner(System.in);
+    int [] arr = {in.nextInt(),in.nextInt(),in.nextInt(),in.nextInt()};
+    Arrays.toString(do_switch(arr));
+
+  }
+  public static int[] do_switch(int[] i) {
+    // your codes here:
+    
+	}
+}
