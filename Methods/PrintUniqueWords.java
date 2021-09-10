9690ca170b8b5f405cc7cff9f85f8abec7de6645
Yahya <yahyahussein91@gmail.com>
2021-09-09 23:06:06 -0700

    called wrong length method

--- Methods/PrintUniqueWords.java
+++ Methods/PrintUniqueWords.java
@@ -9,9 +9,9 @@ public class PrintUniqueWords {
 
 	public static void printUniqueWords(String[] words){
     //WRITE YOUR CODE HERE
-    for (int i = 0; i < words.length(); i++) {
+    for (int i = 0; i < words.length; i++) {
     	int count = 0;
-    	for (int j = 0; j < words.length(); j++) {
+    	for (int j = 0; j < words.length; j++) {
     		if (words[i].equals(words[j])) {
     			count++;
     		}
