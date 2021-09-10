bddfe0b7ae76baf8594fdf0ff902ea6079da83b2
Yahya <yahyahussein91@gmail.com>
2021-09-09 23:10:41 -0700

    import and declare scanner

---
+++ Methods/AddToArray.java
@@ -0,0 +1,11 @@
+/*
+add_to_r is a method that accepts an array and a number.
+
+The method takes the same value of the array but adds the given number into the array at the end. Note: The new number is added which means there is a size difference between the two arrays
+*/
+import java.util.Scanner;
+public class AddToArray {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+}
