1b68d03e3f0c14eb44f78de237b7b303566057ca
Yahya <yahyahussein91@gmail.com>
2021-09-09 23:02:20 -0700

    import and declare scanner object

--- Methods/PrintUniqueWords.java
+++ Methods/PrintUniqueWords.java
@@ -1,3 +1,9 @@
 /*
 Complete the void method printUniqueWords() that will print only unique words from an array of strings. Every single unique word should be printed from the new line.
 */
+import java.util.Scanner;
+public class PrintUniqueWords {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+}
