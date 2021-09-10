2d6de49004943182f05f2c62598253fb7f5f4b1a
Yahya <yahyahussein91@gmail.com>
2021-09-09 22:51:20 -0700

    import and declare scanne obj

--- Methods/PrintUniqueNums.java
+++ Methods/PrintUniqueNums.java
@@ -1,3 +1,9 @@
 /*
-
+Write a void method printUniqueNumbers that will print unique numbers from an array of ints.`
 */
+import java.util.Scanner;
+public class PrintUniqueNums {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+}
