f3dba6cd1d5fb383a7349341ba675b708b1de66a
Yahya <yahyahussein91@gmail.com>
2021-09-09 23:00:15 -0700

    put count in wrong spot all tests passing

--- Methods/PrintUniqueNums.java
+++ Methods/PrintUniqueNums.java
@@ -9,16 +9,16 @@ public class PrintUniqueNums {
 
 	public static void PrintUniqueNumbers(int[] nums){
 	
-		int count = 0;
 		for (int i = 0; i < nums.length; i++) {
+			int count = 0;
 			for (int j = 0; j < nums.length; j++) {
 				if (nums[i] == nums[j]) {
 					count++;
 				}
 			}
-		}
-		if (count == 1) {
-			System.out.println(nums[i]);
+			if (count == 1) {
+				System.out.println(nums[i]);
+			}
 		}
 	}
 }
