bb19331b616832d901ede08095cd7e71877f69f4
Yahya <yahyahussein91@gmail.com>
2021-09-09 22:52:12 -0700

    count to keep track of frequency

--- Methods/PrintUniqueNums.java
+++ Methods/PrintUniqueNums.java
@@ -6,4 +6,10 @@ public class PrintUniqueNums {
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
 	}
+
+	public static void PrintUniqueNumbers(int[] nums){
+
+		int[] result = {};
+		count = 0;
+	}
 }
