e9637bbc23ecd3c32dc30cb93947689f2499a1b9
Yahya <yahyahussein91@gmail.com>
2021-09-09 23:16:03 -0700

    import and declare scanner

--- Methods/CountAppearances.java
+++ Methods/CountAppearances.java
@@ -3,3 +3,9 @@ Create a method that has two parameters of an array of strings and a string and 
 
 It counts how many times the given string appears in the array and returns the count.
 */
+import java.util.Scanner;
+public class CountAppearances {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+}
