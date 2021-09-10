874df489203d3bd81fb0b6af68f475713997c9bc
Yahya <yahyahussein91@gmail.com>
2021-09-09 23:15:25 -0700

    code to count appearance of words

---
+++ Methods/CountAppearances.java
@@ -0,0 +1,5 @@
+/*
+Create a method that has two parameters of an array of strings and a string and should returns an int.
+
+It counts how many times the given string appears in the array and returns the count.
+*/
