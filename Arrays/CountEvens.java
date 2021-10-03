4dac64c581d8a85e31576ac24e384b711a6671ff
Yahya <yahyahussein91@gmail.com>
2021-10-02 22:10:48 -0700

    test is passing

--- Arrays/CountEvens.java
+++ Arrays/CountEvens.java
@@ -8,10 +8,11 @@ public class CountEvens {
 		Scanner input = new Scanner(System.in);
     	int[] nums = {input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};	
     	int count = 0;
-    	for (int i = 0; i < num.length; i++) {
-    		if (nums[i] ) {
-    			
+    	for (int i = 0; i < nums.length; i++) {
+    		if (nums[i] % 2 == 0) {
+    			count++;
     		}
     	}
+    	System.out.println(count);
 	}
 }
