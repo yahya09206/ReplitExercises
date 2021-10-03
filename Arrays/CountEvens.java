c14980d0cb42ef1234ebbe6279f56c29abb75879
Yahya <yahyahussein91@gmail.com>
2021-10-02 22:08:07 -0700

    set up class

---
+++ Arrays/CountEvens.java
@@ -0,0 +1,9 @@
+/*
+Given an array nums, count and print the number of even numbers in the array
+*/
+import java.util.Scanner;
+public class CountEvens {
+	public static void main(String[] args) {
+		
+	}
+}
