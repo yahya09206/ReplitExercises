59d612a7aae4b8104e246d3169abd5cd2342ced2
Yahya <yahyahussein91@gmail.com>
2021-10-02 22:09:06 -0700

    setting up for loop

--- Arrays/CountEvens.java
+++ Arrays/CountEvens.java
@@ -5,5 +5,11 @@ import java.util.Scanner;
 public class CountEvens {
 	public static void main(String[] args) {
 		
+		Scanner input = new Scanner(System.in);
+    	int[] nums = {input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};	
+
+    	for (int i = 0; i < num.length; i++) {
+    		
+    	}
 	}
 }
