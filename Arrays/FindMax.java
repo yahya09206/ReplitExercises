c137a89b4813b73a82b64c2f00b7cfb7c4cdb8bd
Yahya <yahyahussein91@gmail.com>
2021-10-04 23:04:07 -0700

    set up for loop and condition to check

--- Arrays/FindMax.java
+++ Arrays/FindMax.java
@@ -5,5 +5,16 @@ import java.util.*;
 public class FindMax {
 	public static void main(String[] args) {
 		
+		 Scanner input = new Scanner(System.in);
+    	int[] nums = {input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};
+    	// DO NOT TOUCH ABOVE
+    	//TODO: Write your code below.
+    	int max = nums[0];
+    	for (int i = 0; i < nums.length; i++) {
+    		int each = nums[i];
+    		if (each > nums[i]) {
+    			max = each;
+    		}
+    	}
 	}
 }
