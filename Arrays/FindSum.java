fea962ad76672d8b28243e8edd8cba52a5a314b8
Yahya <yahyahussein91@gmail.com>
2021-10-04 19:52:50 -0700

    import scanner obj

--- Arrays/FindSum.java
+++ Arrays/FindSum.java
@@ -2,8 +2,11 @@
 Given an array num, calculate the sum of all numbers in the array and print out to the console.
 */
 import java.util.Arrays;
+import java.util.Scanner;
 public class FindSum {
 	public static void main(String[] args) {
 		
+		Scanner input = new Scanner(System.in);
+    	int[] nums = {input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};
 	}
 }
