569ab43cf0c8109adfdc6fb7eedfc4b3f25589a3
Yahya <yahyahussein91@gmail.com>
2021-10-04 22:20:47 -0700

    code to print first two elements

---
+++ Arrays/FrontPiece.java
@@ -0,0 +1,9 @@
+/*
+Given an int array num of any length, print a new array of its first 2 elements. If the array is smaller than length 2, use whatever elements are present.
+*/
+import java.util.*;
+public class FrontPiece {
+	public static void main(String[] args) {
+		
+	}
+}
