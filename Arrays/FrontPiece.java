363ca3037aa8f1cb9cb10b743a2d286ab896da0e
Yahya <yahyahussein91@gmail.com>
2021-10-04 22:40:37 -0700

    only working for size less than 2

--- Arrays/FrontPiece.java
+++ Arrays/FrontPiece.java
@@ -5,5 +5,19 @@ import java.util.*;
 public class FrontPiece {
 	public static void main(String[] args) {
 		
+		Scanner scan = new Scanner(System.in);
+		int size = scan.nextInt();
+		int[] num = new int[size];
+		for(int i =0; i < size; i++) {
+			num[i] = scan.nextInt();
+		}
+
+		if (num.length < 2) {
+			System.out.println(Arrays.toString(num));
+		}else {
+      		int[] num2 = new int[num[0] + num[1]];
+      		System.out.println(Arrays.toString(num2));
+    	}
+
 	}
 }
