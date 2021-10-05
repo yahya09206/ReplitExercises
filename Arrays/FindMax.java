b659217171f0efa2e8c5841cd962e46a06ec6a65
Yahya <yahyahussein91@gmail.com>
2021-10-04 23:02:30 -0700

    code to find max element in array

---
+++ Arrays/FindMax.java
@@ -0,0 +1,9 @@
+/*
+Given an array num, get the max number in the array and print it out to the console.
+*/
+import java.util.*;
+public class FindMax {
+	public static void main(String[] args) {
+		
+	}
+}
