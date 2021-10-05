d6a1e74ca5a1b85d681857cc9a86da2a66883abb
Yahya <yahyahussein91@gmail.com>
2021-10-04 19:52:17 -0700

    set up class

--- Arrays/FindSum.java
+++ Arrays/FindSum.java
@@ -1,3 +1,9 @@
 /*
 Given an array num, calculate the sum of all numbers in the array and print out to the console.
 */
+import java.util.Arrays;
+public class FindSum {
+	public static void main(String[] args) {
+		
+	}
+}
