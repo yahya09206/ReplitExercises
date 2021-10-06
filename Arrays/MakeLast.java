ff6b4d121aca19055f8777b8aaa9126c30cf7d7f
Yahya <yahyahussein91@gmail.com>
2021-10-05 21:41:57 -0700

    code to insert value into last index in array

---
+++ Arrays/MakeLast.java
@@ -0,0 +1,7 @@
+/*
+Given an int array nums print a new array with double the length where its last element is the same as the original array, and all the other elements are 0. The original array will be length 1 or more. Hint: by default, a new int array contains all 0's.
+*/
+import java.util.Arrays;
+public class MakeLast {
+	
+}
