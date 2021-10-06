2b9f38c896bf3e85b0f06c017f886a4159f16a46
Yahya <yahyahussein91@gmail.com>
2021-10-05 22:16:32 -0700

    completely wrong approach smh

--- Arrays/PrintFirstAndLast.java
+++ Arrays/PrintFirstAndLast.java
@@ -11,10 +11,9 @@ public class PrintFirstAndLast {
     	String[] words = {input.nextLine(),input.nextLine(),input.nextLine(),input.nextLine(),input.nextLine()};
 
     	// iterate through values
-    	for (int i = 0; i < words.length() - 1; i++) {
-    		int[] first = new int[2];
-    		first[0] = words[0];
-    		first[1] = words[words.length];
+    	for (int i = 0; i < words.length - 1; i++) {
+    		String eachWord = words[i];
+            System.out.println("" + eachWord.charAt(0)+eachWord.charAt(eachWord.length()-1));
     	}
 	}
 }
