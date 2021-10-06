65a0d682643f3d5e89dd207b88cf46362b16f8be
Yahya <yahyahussein91@gmail.com>
2021-10-05 21:42:41 -0700

    copied over boiler code

--- Arrays/MakeLast.java
+++ Arrays/MakeLast.java
@@ -3,5 +3,15 @@ Given an int array nums print a new array with double the length where its last 
 */
 import java.util.Arrays;
 public class MakeLast {
-	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+    	int size = scan.nextInt();
+    	int[] nums = new int[size];
+		for(int i =0; i < size; i++) {
+			nums[i] = scan.nextInt();
+		}
+
+		
+	}
 }
