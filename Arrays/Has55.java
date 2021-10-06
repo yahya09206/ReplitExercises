8fe837e3b42fdf36fa17daab69700b40a807ed0d
Yahya <yahyahussein91@gmail.com>
2021-10-05 22:00:04 -0700

    set up class

--- Arrays/Has55.java
+++ Arrays/Has55.java
@@ -1,3 +1,9 @@
 /*
 Given an array nums, print true if the array contains a 5 next to a 5 anywhere in the array. If no consecutive 5s or no 5s are detected in your code then print false.
 */
+import java.util.Arrays;
+public class Has55 {
+	public static void main(String[] args) {
+		
+	}
+}
