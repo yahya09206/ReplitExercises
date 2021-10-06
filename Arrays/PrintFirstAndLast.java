28317f00bda2fe86cbe2f5cd208d0cd40f40483d
Yahya <yahyahussein91@gmail.com>
2021-10-05 22:14:31 -0700

    declare new array to store values

--- Arrays/PrintFirstAndLast.java
+++ Arrays/PrintFirstAndLast.java
@@ -7,5 +7,14 @@ import java.util.Scanner;
 public class PrintFirstAndLast {
 	public static void main(String[] args) {
 		
+		Scanner input = new Scanner(System.in);
+    	String[] words = {input.nextLine(),input.nextLine(),input.nextLine(),input.nextLine(),input.nextLine()};
+
+    	// iterate through values
+    	for (int i = 0; i < words.length() - 1; i++) {
+    		int[] first = new int[2];
+    		first[0] = words[0];
+    		first[1] = words[words.length];
+    	}
 	}
 }
