a7b8b2a7d4c4cbbe4e9dc7df74bcd64c4a5a58a7
Yahya <yahyahussein91@gmail.com>
2021-10-05 21:47:55 -0700

    set up template for class

--- Arrays/AvgTemps.java
+++ Arrays/AvgTemps.java
@@ -1,3 +1,14 @@
 /*
 Given an array temps with temperature values find and print the average value
 */
+import java.util.Arrays;
+public AvgTemps {
+	public static void main(String[] args) {
+		
+		 Scanner scan = new Scanner(System.in);
+		double[] temps = { scan.nextDouble(), scan.nextDouble(), scan.nextDouble(), scan.nextDouble(), scan.nextDouble(), scan.nextDouble(), scan.nextDouble() , scan.nextDouble()  };
+
+		int sum = 0;
+		
+	}
+}
