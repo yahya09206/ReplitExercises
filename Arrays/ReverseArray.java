dac8e5f6dd9c7d23bc2a0a67126220148fc6ac2c
Yahya <yahyahussein91@gmail.com>
2021-10-26 22:23:58 -0700

    use pseudocode

--- Arrays/ReverseArray.java
+++ Arrays/ReverseArray.java
@@ -3,7 +3,9 @@ public class ReverseArray {
 	public static void main(String[] args) {
 		
 		for (int i = nums.length - 1; i > 0; i--) {
-				
-			}	
+			int temp = nums[i];
+            nums[i] = nums[nums.length-(i+1)];
+            nums[nums.length-(i+1)] = temp;		
+		}	
 	}
 }
