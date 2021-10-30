ea30f0d275c7437539928128baf3fb97ea636ad3
Yahya <yahyahussein91@gmail.com>
2021-10-29 21:01:26 -0700

    imported scanner and set up prompt

--- Methods/ReverseString.java
+++ Methods/ReverseString.java
@@ -1,6 +1,9 @@
 //The reverse method will reverse the given String
+import java.util.Scanner;
 public class ReverseString{
 	public static void main(String[] args) {
 		
+		Scanner in = new Scanner(System.in);
+		System.out.println(reverse(in.next()));
 	}
 }
