32174516f294ca11d949c9a0280650121977c34b
Yahya <yahyahussein91@gmail.com>
2021-12-11 20:24:59 -0800

    corrected error

--- Methods/ReverseString.java
+++ Methods/ReverseString.java
@@ -10,7 +10,7 @@ public class ReverseString{
 
 	public static String reverseString(String word){
 		String result = "";
-		for (int i = word.length() - 1; i > 0; i--) {
+		for (int i = word.length() - 1; i >= 0; i--) {
 			result += word.charAt(i);
 		}
 		return result;
