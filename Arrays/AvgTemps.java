58d7e7f86bfc7516aa2c3ca893ac285b42b0b68e
Yahya <yahyahussein91@gmail.com>
2021-12-23 21:03:06 -0800

    restarting array replits

--- Arrays/AvgTemps.java
+++ Arrays/AvgTemps.java
@@ -1,20 +1,10 @@
 /*
 Given an array temps with temperature values find and print the average value
 */
-import java.util.Arrays;
+
 public AvgTemps {
 	public static void main(String[] args) {
 		
-		 Scanner scan = new Scanner(System.in);
-		double[] temps = { scan.nextDouble(), scan.nextDouble(), scan.nextDouble(), scan.nextDouble(), scan.nextDouble(), scan.nextDouble(), scan.nextDouble() , scan.nextDouble()  };
-
-
-		// do like this for future reference
-		double sum = 0;
-		for (int i = 0; i < temps.length; i++) {
-			sum += temps[i];
-		}
 		
-		System.out.println(sum / temps.length);
 	}
 }
