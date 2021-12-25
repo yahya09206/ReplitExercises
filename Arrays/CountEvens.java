f51f1b3290c2e4c216416876538e60e9cc1b44b2
Yahya <yahyahussein91@gmail.com>
2021-12-24 19:30:51 -0800

    restarting

--- Arrays/CountEvens.java
+++ Arrays/CountEvens.java
@@ -5,14 +5,6 @@ import java.util.Scanner;
 public class CountEvens {
 	public static void main(String[] args) {
 		
-		Scanner input = new Scanner(System.in);
-    	int[] nums = {input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};	
-    	int count = 0;
-    	for (int i = 0; i < nums.length; i++) {
-    		if (nums[i] % 2 == 0) {
-    			count++;
-    		}
-    	}
-    	System.out.println(count);
+	
 	}
 }
