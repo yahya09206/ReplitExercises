1eb0f62c4a830b4503949cea7f67659310989f3f
Yahya <yahyahussein91@gmail.com>
2021-12-25 14:34:23 -0800

    still cleaning

--- Arrays/FindMax.java
+++ Arrays/FindMax.java
@@ -5,17 +5,6 @@ import java.util.*;
 public class FindMax {
 	public static void main(String[] args) {
 		
-		 Scanner input = new Scanner(System.in);
-    	int[] nums = {input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};
-    	// DO NOT TOUCH ABOVE
-    	//TODO: Write your code below.
-    	int max = nums[0];
-    	for (int i = 0; i < nums.length; i++) {
-    		int each = nums[i];
-    		if (each > max) {
-    			max = each;
-    		}
-    	}
-    	System.out.println(max);
+		 
 	}
 }
