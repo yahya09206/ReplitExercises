c0faac5a2c97b7d94530337c6c8d53cfe90d7124
Yahya <yahyahussein91@gmail.com>
2021-12-26 23:27:28 -0800

    cleaning up

--- Arrays/FindSum.java
+++ Arrays/FindSum.java
@@ -6,13 +6,6 @@ import java.util.Scanner;
 public class FindSum {
 	public static void main(String[] args) {
 		
-		Scanner input = new Scanner(System.in);
-    	int[] nums = {input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};
-
-    	int sum = 0;
-    	for (int i = 0; i < nums.length; i++) {
-    		sum += nums[i];
-    	}
-    	System.out.println(sum);
+	
 	}
 }
