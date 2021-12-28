c4f689ba4f1b8072b45beb6552794df062b2d278
Yahya <yahyahussein91@gmail.com>
2021-12-27 20:13:45 -0800

    last day of cleaning

--- Arrays/FrontPiece.java
+++ Arrays/FrontPiece.java
@@ -5,21 +5,7 @@ import java.util.*;
 public class FrontPiece {
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
-		int size = scan.nextInt();
-		int[] num = new int[size];
-		for(int i =0; i < size; i++) {
-			num[i] = scan.nextInt();
-		}
-
-		if (num.length < 2) {
-			System.out.println(Arrays.toString(num));
-		}else {
-      		int[] num2 = new int[2];
-      		num2[0] = num[0];
-      		num2[1] = num[1];
-      		System.out.println(Arrays.toString(num2));
-    	}
+		
 
 	}
 }
